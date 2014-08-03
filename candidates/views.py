cfab9c0502e75fb534da0d5e5b497be3926d2c4d
Mark Longair <mhl@pobox.com>
2014-08-03 13:08:12 +0100

    Add some error handling for invalid postcodes
    Pair-programmed with Jenny Duckett (@jennyd)

--- candidates/views.py
+++ candidates/views.py
@@ -5,6 +5,7 @@ import requests
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.utils.http import urlquote
 from django.views.generic import FormView, TemplateView
 
 from .forms import PostcodeForm
@@ -17,15 +18,26 @@ class ConstituencyFinderView(FormView):
         postcode = form.cleaned_data['postcode']
         url = 'http://mapit.mysociety.org/postcode/' + postcode
         r = requests.get(url)
-        mapit_result = r.json
-        mapit_constituency_id = mapit_result['shortcuts']['WMC']
-        mapit_constituency_data = mapit_result['areas'][str(mapit_constituency_id)]
-        constituency_url = reverse(
-            'constituency',
-            kwargs={'constituency_name': mapit_constituency_data['name']}
-        )
-        return HttpResponseRedirect(constituency_url)
+        if r.status_code == 200:
+            mapit_result = r.json
+            mapit_constituency_id = mapit_result['shortcuts']['WMC']
+            mapit_constituency_data = mapit_result['areas'][str(mapit_constituency_id)]
+            constituency_url = reverse(
+                'constituency',
+                kwargs={'constituency_name': mapit_constituency_data['name']}
+            )
+            return HttpResponseRedirect(constituency_url)
+        else:
+            error_url = reverse('finder')
+            error_url += '?bad_postcode=' + urlquote(postcode)
+            return HttpResponseRedirect(error_url)
 
+    def get_context_data(self, **kwargs):
+        context = super(ConstituencyFinderView, self).get_context_data(**kwargs)
+        bad_postcode = self.request.GET.get('bad_postcode')
+        if bad_postcode:
+            context['bad_postcode'] = bad_postcode
+        return context
 
 class ConstituencyDetailView(TemplateView):
     template_name = 'candidates/constituency.html'
