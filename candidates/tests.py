a77a1085838a57ec12a7b86a16794853e34154da
Mark Longair <mhl@pobox.com>
2014-09-25 15:37:28 +0100

    Mock out the PopIt API object for the constituency smoke test

--- candidates/tests.py
+++ candidates/tests.py
@@ -1,6 +1,10 @@
+import json
+from os.path import dirname, join
 from urlparse import urlsplit
 
 from django_webtest import WebTest
+
+from mock import patch, MagicMock
 
 class TestConstituencyPostcodeFinderView(WebTest):
 
@@ -48,9 +52,41 @@ class TestConstituencyPostcodeFinderView(WebTest):
         self.assertEqual(split_location.query, 'bad_postcode=foo%20bar')
 
 
+class FakeCollection(object):
+
+    example_popit_data_directory = join(
+        dirname(__file__), 'example-popit-data'
+    )
+
+    def __init__(self, *args):
+        self.object_id = args[0] if len(args) == 1 else None
+
+    def get(self, **kwargs):
+        with open(join(
+                self.example_popit_data_directory,
+                '{0}_{1}_embed={2}.json'.format(
+                    self.collection,
+                    self.object_id,
+                    kwargs.get('embed', 'membership')
+                )
+        )) as f:
+                return json.load(f)
+
+
+class FakePersonCollection(FakeCollection):
+    collection = 'persons'
+
+
+class FakeOrganizationCollection(FakeCollection):
+    collection = 'organizations'
+
+
 class TestConstituencyDetailView(WebTest):
 
-    def test_any_constituency_page(self):
+    @patch('candidates.views.PopIt')
+    def test_any_constituency_page(self, MockPopIt):
+        MockPopIt.return_value.organizations = FakeOrganizationCollection
+        MockPopIt.return_value.persons = FakePersonCollection
         # Just a smoke test for the moment:
         response = self.app.get('/constituency/65808/dulwich-and-west-norwood')
         response.mustcontain('Tessa Jowell (Labour Party)')
