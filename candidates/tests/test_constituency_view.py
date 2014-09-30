c2b4b758c2686af810ff5a9c26218772bb6b09e4
Mark Longair <mhl@pobox.com>
2014-09-30 15:38:46 +0100

    Test the constituency page both when logged in and not
    There should be no forms on the page when not logged in, and make sure
    that at least the "Add Candidate" form is there if logged in.

--- candidates/tests/test_constituency_view.py
+++ candidates/tests/test_constituency_view.py
@@ -2,14 +2,29 @@ from mock import patch
 
 from django_webtest import WebTest
 
+from .auth import TestUserMixin
 from .fake_popit import FakePersonCollection, FakeOrganizationCollection
 
-class TestConstituencyDetailView(WebTest):
+@patch('candidates.views.PopIt')
+class TestConstituencyDetailView(TestUserMixin, WebTest):
 
-    @patch('candidates.views.PopIt')
-    def test_any_constituency_page(self, mock_popit):
+    def test_any_constituency_page_without_login(self, mock_popit):
         mock_popit.return_value.organizations = FakeOrganizationCollection
         mock_popit.return_value.persons = FakePersonCollection
         # Just a smoke test for the moment:
         response = self.app.get('/constituency/65808/dulwich-and-west-norwood')
         response.mustcontain('Tessa Jowell (Labour Party)')
+        # There should be no forms on the page if you're not logged in:
+        self.assertEqual(0, len(response.forms))
+
+    def test_any_constituency_page(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        # Just a smoke test for the moment:
+        response = self.app.get(
+            '/constituency/65808/dulwich-and-west-norwood',
+            user=self.user
+        )
+        response.mustcontain('Tessa Jowell (Labour Party)')
+        form = response.forms['new-candidate-form']
+        self.assertTrue(form)
