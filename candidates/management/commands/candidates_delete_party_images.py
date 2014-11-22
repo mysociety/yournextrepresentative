ff6ab69e57ef5c2d9c5299448231054c58dbbfd7
Mark Longair <mhl@pobox.com>
2014-11-22 11:45:37 +0000

    Add a command to delete all images from the 'images' array of each party

---
+++ candidates/management/commands/candidates_delete_party_images.py
@@ -0,0 +1,19 @@
+import json
+
+from candidates.popit import PopItApiMixin, popit_unwrap_pagination
+
+from django.core.management.base import BaseCommand
+
+class Command(PopItApiMixin, BaseCommand):
+
+    def handle(self, **options):
+        for o in popit_unwrap_pagination(
+                self.api.organizations,
+                per_page=100
+        ):
+            if o['classification'] != 'Party':
+                continue
+            print o['name']
+            for image in o.get('images', []):
+                print "  DELETE", image['_id']
+                self.api.organizations(o['id']).image(image['_id']).delete()
