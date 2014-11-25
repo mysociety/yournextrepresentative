db6c7d8c368fc171a8897ea8574d79ff766da88f
Mark Longair <mhl@pobox.com>
2014-11-25 18:59:55 +0000

    Display the action log in the admin interface

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.forms import ModelForm
 
+from .models import LoggedAction
 # Register your models here.
+
+class LoggedActionAdminForm(ModelForm):
+    pass
+
+class LoggedActionAdmin(admin.ModelAdmin):
+    form = LoggedActionAdminForm
+    list_display = ['user', 'action_type', 'popit_person_new_version',
+                    'popit_person_id', 'created', 'updated', 'source']
+    ordering = ('-created',)
+
+admin.site.register(LoggedAction, LoggedActionAdmin)
