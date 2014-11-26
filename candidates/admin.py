22bdfc07a2e8f9aa4076b531b9a1134875180fb6
Mark Longair <mhl@pobox.com>
2014-11-26 16:51:11 +0000

    Linkify the person ID in the action log

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from django.core.urlresolvers import reverse
 from django.forms import ModelForm
 
 from .models import LoggedAction
@@ -10,7 +12,15 @@ class LoggedActionAdminForm(ModelForm):
 class LoggedActionAdmin(admin.ModelAdmin):
     form = LoggedActionAdminForm
     list_display = ['user', 'action_type', 'popit_person_new_version',
-                    'popit_person_id', 'created', 'updated', 'source']
+                    'popit_person_link', 'created', 'updated', 'source']
     ordering = ('-created',)
+
+    def popit_person_link(self, o):
+        url = reverse('person-view', kwargs={'person_id': o.popit_person_id})
+        return '<a href="{0}">{1}</a>'.format(
+            url,
+            o.popit_person_id,
+        )
+    popit_person_link.allow_tags = True
 
 admin.site.register(LoggedAction, LoggedActionAdmin)
