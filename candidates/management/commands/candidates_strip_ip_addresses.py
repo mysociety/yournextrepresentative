1b7f6ca19e395f2285556ae43c97eb761dc9e565
Mark Longair <mhl@pobox.com>
2014-11-27 14:45:09 +0000

    Add a command to strip the user IP addresses from PopIt

---
+++ candidates/management/commands/candidates_strip_ip_addresses.py
@@ -0,0 +1,17 @@
+from candidates.popit import PopItApiMixin, popit_unwrap_pagination
+
+from django.core.management.base import BaseCommand
+
+class Command(PopItApiMixin, BaseCommand):
+
+    def handle(self, **options):
+        for person in popit_unwrap_pagination(
+                self.api.persons,
+                per_page=100
+        ):
+            print "Stripping IP addresses from {name} ({id})".format(
+                **person
+            )
+            for version in person.get('versions', []):
+                version.pop('ip', None)
+            self.api.persons(person['id']).put(person)
