d89df41e0546d6c1c8bc43df0ef9a1e15a641a78
Mark Longair <mhl@pobox.com>
2014-11-28 20:03:24 +0000

    Don't allow @parliament.uk email addresses
    As various people have pointed out in #63, when parliament dissolves
    before the election the @parliament.uk email addresses shouldn't really
    be used, so we should try to collect alternative email addresses instead
    of the parliament.uk ones. This commit adds a validator that checks that
    the email address isn't an @parliament.uk one.

--- candidates/tests/test_validators.py
+++ candidates/tests/test_validators.py
@@ -41,4 +41,33 @@ class TestValidators(TestCase):
             form.cleaned_data['twitter_username'],
             'madeuptwitteraccount'
         )
-        
+
+    def test_non_parliament_email(self):
+        form = BasePersonForm({
+            'name': 'John Doe',
+            'email': 'foo@example.org',
+        })
+        self.assertTrue(form.is_valid())
+        self.assertEqual(
+            form.cleaned_data['email'],
+            'foo@example.org',
+        )
+
+    def test_parliament_email(self):
+        form = BasePersonForm({
+            'name': 'John Bercow',
+            'email': 'john.bercow.mp@parliament.uk',
+        })
+        self.assertFalse(form.is_valid())
+        error_message = u"parliament.uk email addresses aren't useful once "
+        error_message += u"parliament dissolves; please try to find an "
+        error_message += u"alternative"
+        self.assertEqual(form.errors, {'email': [error_message]})
+
+    def test_malformed_email(self):
+        form = BasePersonForm({
+            'name': 'John Bercow',
+            'email': 'foo bar!',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'email': ['Enter a valid email address.']})
