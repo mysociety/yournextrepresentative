a233e95a54de1a62d1ca149554e0cb8d0f3b35e3
Sym Roe <sym.roe@talusdesign.co.uk>
2014-12-14 16:22:07 +0000

    Refactor test to allow calling from elsewhere
    It's useful to test creating a person from other places, so mock_create_person
    does all the hard work, leaving the test case to just test for person creation

--- candidates/tests/test_create_person.py
+++ candidates/tests/test_create_person.py
@@ -12,157 +12,164 @@ from ..views import PersonUpdateMixin, CandidacyMixin, PopItApiMixin
 class MinimalUpdateClass(PersonUpdateMixin, CandidacyMixin, PopItApiMixin):
     pass
 
-class TestCreatePerson(TestCase):
-
-    @patch.object(FakePersonCollection, 'post')
-    @patch('candidates.views.requests')
-    @patch('candidates.popit.PopIt')
-    def test_create_jane_doe(self, mock_popit, mock_requests, mocked_post):
-
-        mocked_post.return_value = {
-            'result': {
-                'id': 'jane-doe'
+EXPECTED_ARGS = {
+    'birth_date': None,
+    'email': u'jane@example.org',
+    'gender': 'female',
+    'id': '1',
+    'identifiers': [
+        {
+            'scheme': 'yournextmp-candidate',
+            'identifier': '1234567'
+        }
+    ],
+    'links': [
+        {
+            'note': 'facebook page',
+            'url': 'http://notreallyfacebook/tessajowellcampaign',
+        },
+        {
+            'note': 'party PPC page',
+            'url': 'http://labour.example.org/tessajowell',
+        },
+        {
+            'note': 'facebook personal',
+            'url': 'http://notreallyfacebook/tessajowell',
+        },
+        {
+            'note': 'homepage',
+            'url': 'http://janedoe.example.org'
+        },
+    ],
+    'name': u'Jane Doe',
+    'party_memberships': {
+        '2015': {
+            'id': 'party:53',
+            'name': 'Labour Party'
+        }
+    },
+    'standing_in': {
+        '2015': {
+            'name': 'Dulwich and West Norwood',
+            'mapit_url': 'http://mapit.mysociety.org/area/65808'}
+    },
+    'versions': [
+        {
+            'information_source': 'A change made for testing purposes',
+            'username': 'tester',
+            'version_id': '6054aa38b30b4418',
+            'timestamp': '2014-09-28T14:02:44.567413',
+            'data': {
+                'twitter_username': '',
+                'standing_in': {
+                    '2015': {
+                        'name': 'Dulwich and West Norwood',
+                        'mapit_url': 'http://mapit.mysociety.org/area/65808'
+                    }
+                },
+                'homepage_url': 'http://janedoe.example.org',
+                'identifiers': [
+                    {
+                        'scheme': 'yournextmp-candidate',
+                        'identifier': '1234567'
+                    }
+                ],
+                'facebook_page_url': 'http://notreallyfacebook/tessajowellcampaign',
+                'facebook_personal_url': 'http://notreallyfacebook/tessajowell',
+                'party_ppc_page_url': 'http://labour.example.org/tessajowell',
+                'birth_date': None,
+                'gender': 'female',
+                'name': 'Jane Doe',
+                'wikipedia_url': '',
+                'party_memberships': {
+                    '2015': {
+                        'id': 'party:53',
+                        'name': 'Labour Party'
+                    }
+                },
+                'email': 'jane@example.org',
+                'id': '1'
             }
         }
+    ],
+}
 
-        mock_requests.get = fake_get_result
-
-        mock_popit.return_value.organizations = FakeOrganizationCollection
-        mock_popit.return_value.persons = FakePersonCollection
-
-        view = MinimalUpdateClass()
-
-        view.create_candidate_list_memberships = MagicMock()
-        view.create_party_memberships = MagicMock()
-
-        new_person_data = {
-            "birth_date": None,
-            "email": "jane@example.org",
-            "gender": "female",
-            "homepage_url": "http://janedoe.example.org",
-            "identifiers": [
-                {
-                    "scheme": "yournextmp-candidate",
-                    "identifier": "1234567"
-                }
-            ],
-            "name": "Jane Doe",
-            "party_memberships": {
-                "2015": {
-                    "id": "party:53",
-                    "name": "Labour Party"
-                }
-            },
-            "standing_in": {
-                "2015": {
-                    "mapit_url": "http://mapit.mysociety.org/area/65808",
-                    "name": "Dulwich and West Norwood"
-                }
-            },
-            "twitter_username": "",
-            "wikipedia_url": "",
-            "facebook_personal_url": "http://notreallyfacebook/tessajowell",
-            "facebook_page_url": "http://notreallyfacebook/tessajowellcampaign",
-            "party_ppc_page_url": "http://labour.example.org/tessajowell",
+NEW_PERSON_DATA = {
+    "birth_date": None,
+    "email": "jane@example.org",
+    "gender": "female",
+    "homepage_url": "http://janedoe.example.org",
+    "identifiers": [
+        {
+            "scheme": "yournextmp-candidate",
+            "identifier": "1234567"
         }
-
-        view.create_person(
-            new_person_data,
-            {
-                'information_source': 'A change made for testing purposes',
-                'username': 'tester',
-                'version_id': '6054aa38b30b4418',
-                'timestamp': '2014-09-28T14:02:44.567413',
-            },
-        )
-
-        expected_args = {
-            'birth_date': None,
-            'email': u'jane@example.org',
-            'gender': 'female',
-            'id': '1',
-            'identifiers': [
-                {
-                    'scheme': 'yournextmp-candidate',
-                    'identifier': '1234567'
-                }
-            ],
-            'links': [
-                {
-                    'note': 'facebook page',
-                    'url': 'http://notreallyfacebook/tessajowellcampaign',
-                },
-                {
-                    'note': 'party PPC page',
-                    'url': 'http://labour.example.org/tessajowell',
-                },
-                {
-                    'note': 'facebook personal',
-                    'url': 'http://notreallyfacebook/tessajowell',
-                },
-                {
-                    'note': 'homepage',
-                    'url': 'http://janedoe.example.org'
-                },
-            ],
-            'name': u'Jane Doe',
-            'party_memberships': {
-                '2015': {
-                    'id': 'party:53',
-                    'name': 'Labour Party'
-                }
-            },
-            'standing_in': {
-                '2015': {
-                    'name': 'Dulwich and West Norwood',
-                    'mapit_url': 'http://mapit.mysociety.org/area/65808'}
-            },
-            'versions': [
-                {
-                    'information_source': 'A change made for testing purposes',
-                    'username': 'tester',
-                    'version_id': '6054aa38b30b4418',
-                    'timestamp': '2014-09-28T14:02:44.567413',
-                    'data': {
-                        'twitter_username': '',
-                        'standing_in': {
-                            '2015': {
-                                'name': 'Dulwich and West Norwood',
-                                'mapit_url': 'http://mapit.mysociety.org/area/65808'
-                            }
-                        },
-                        'homepage_url': 'http://janedoe.example.org',
-                        'identifiers': [
-                            {
-                                'scheme': 'yournextmp-candidate',
-                                'identifier': '1234567'
-                            }
-                        ],
-                        'facebook_page_url': 'http://notreallyfacebook/tessajowellcampaign',
-                        'facebook_personal_url': 'http://notreallyfacebook/tessajowell',
-                        'party_ppc_page_url': 'http://labour.example.org/tessajowell',
-                        'birth_date': None,
-                        'gender': 'female',
-                        'name': 'Jane Doe',
-                        'wikipedia_url': '',
-                        'party_memberships': {
-                            '2015': {
-                                'id': 'party:53',
-                                'name': 'Labour Party'
-                            }
-                        },
-                        'email': 'jane@example.org',
-                        'id': '1'
-                    }
-                }
-            ],
+    ],
+    "name": "Jane Doe",
+    "party_memberships": {
+        "2015": {
+            "id": "party:53",
+            "name": "Labour Party"
         }
+    },
+    "standing_in": {
+        "2015": {
+            "mapit_url": "http://mapit.mysociety.org/area/65808",
+            "name": "Dulwich and West Norwood"
+        }
+    },
+    "twitter_username": "",
+    "wikipedia_url": "",
+    "facebook_personal_url": "http://notreallyfacebook/tessajowell",
+    "facebook_page_url": "http://notreallyfacebook/tessajowellcampaign",
+    "party_ppc_page_url": "http://labour.example.org/tessajowell",
+}
 
+@patch.object(FakePersonCollection, 'post')
+@patch('candidates.views.requests')
+@patch('candidates.popit.PopIt')
+def mock_create_person(mock_popit, mock_requests, mocked_post):
+    mocked_post.return_value = {
+        'result': {
+            'id': 'jane-doe'
+        }
+    }
+
+    mock_requests.get = fake_get_result
+
+    mock_popit.return_value.organizations = FakeOrganizationCollection
+    mock_popit.return_value.persons = FakePersonCollection
+
+    view = MinimalUpdateClass()
+
+    view.create_candidate_list_memberships = MagicMock()
+    view.create_party_memberships = MagicMock()
+
+
+
+    view.create_person(
+        NEW_PERSON_DATA,
+        {
+            'information_source': 'A change made for testing purposes',
+            'username': 'tester',
+            'version_id': '6054aa38b30b4418',
+            'timestamp': '2014-09-28T14:02:44.567413',
+        },
+    )
+
+    return mocked_post, view
+
+
+class TestCreatePerson(TestCase):
+
+    def test_create_jane_doe(self):
+
+        (mocked_post, view) = mock_create_person()
         # Then we expect one post, with the right data:
         self.assertEqual(1, len(mocked_post.call_args_list))
         self.assertTrue(
             equal_call_args(
-                [expected_args],
+                [EXPECTED_ARGS],
                 mocked_post.call_args[0]
             ),
             "update_person was called with unexpected values"
@@ -170,9 +177,9 @@ class TestCreatePerson(TestCase):
 
         view.create_candidate_list_memberships.assert_called_once_with(
             'jane-doe',
-            new_person_data,
+            NEW_PERSON_DATA,
         )
         view.create_party_memberships.assert_called_once_with(
             'jane-doe',
-            new_person_data
+            NEW_PERSON_DATA
         )
