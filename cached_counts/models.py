423eff7a4f0d205af26b157451acde41992cb5e8
Sym Roe <sym.roe@talusdesign.co.uk>
2014-12-14 16:19:35 +0000

    Emit a person_added signal on creating new people

--- cached_counts/models.py
+++ cached_counts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.template.defaultfilters import slugify
 
+from candidates.models import PopItPerson
+
 class CachedCount(models.Model):
     """
     Fairly generic model for storing counts of various sorts.
@@ -29,3 +31,39 @@ class CachedCount(models.Model):
                 'mapit_area_id': self.object_id,
                 'ignored_slug': slugify(self.name)
             })
+
+    @classmethod
+    def increment_count(cls, count_type, object_id):
+        """
+        Increments the count of the object with the type of `count_type` and
+        the id of `object_id`.  If this object does not exist, do nothing.
+        """
+        filters = {
+            'count_type': count_type,
+            'object_id': object_id,
+        }
+
+        cls.objects.filter(**filters).update(count=models.F('count') + 1)
+
+
+
+from candidates.update import person_added
+from django.dispatch import receiver
+
+@receiver(person_added, sender=PopItPerson)
+def person_added_handler(sender, **kwargs):
+    """
+    Called when the `person_added` signal is sent, mainly from
+    `candidates.update.create_person`.
+    """
+
+    data = kwargs['data']
+
+    # constituency
+    constituency_url = data['standing_in'].get('2015', {}).get('mapit_url')
+    constituency_id = constituency_url.split('/')[-1]
+    CachedCount.increment_count('constituency', constituency_id)
+
+    # party
+    party_id = data['party_memberships'].get('2015', {}).get('id')
+    CachedCount.increment_count('party', party_id)
