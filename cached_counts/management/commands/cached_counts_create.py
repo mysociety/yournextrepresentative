94be76985f6aaec88e81ea04cdc6e7914940ed7f
Sym Roe <sym.roe@talusdesign.co.uk>
2014-12-16 19:22:59 +0000

    Add new counts to cache table

--- cached_counts/management/commands/cached_counts_create.py
+++ cached_counts/management/commands/cached_counts_create.py
@@ -29,13 +29,17 @@ class Command(PopItApiMixin, BaseCommand):
             'constituencies': {n[1]['name']: {'count': 0, 'con_id':n[1]['id']}
                                 for n in all_constituencies},
         }
-
+        count_2010 = 0
+        standing_again = 0
+        all_people = 0
+        new_candidates = 0
         # Loop over everything, counting things we're interseted in
         for person in popit_unwrap_pagination(
                 self.api.persons,
                 embed="membership.organization",
                 per_page=100
         ):
+            all_people += 1
             if person.get('standing_in') and person['standing_in'].get('2015'):
                 counts['candidates_2015'] += 1
 
@@ -44,6 +48,15 @@ class Command(PopItApiMixin, BaseCommand):
 
                 constituency_name = person['standing_in']['2015']['name']
                 counts['constituencies'][constituency_name]['count'] += 1
+                if person['standing_in'].get('2010'):
+                    standing_again += 1
+            if person.get('standing_in') and person['standing_in'].get('2010'):
+                count_2010 +=1
+            else:
+                new_candidates += 1
+
+            if not person.get('standing_in'):
+                print person
 
         # Add or create objects in the database
         # Parties
@@ -70,10 +83,29 @@ class Command(PopItApiMixin, BaseCommand):
 
             self.add_or_update(obj)
 
+        # Total candidates in 2015
         obj = {
             'count_type': 'total',
             'name': 'total_2015',
             'count': counts['candidates_2015'],
             'object_id': 'candidates_2015',
+        }
+        self.add_or_update(obj)
+
+        # New candidates
+        obj = {
+            'count_type': 'total',
+            'name': 'new_candidates',
+            'count': new_candidates,
+            'object_id': 'new_candidates',
+        }
+        self.add_or_update(obj)
+
+        # Standing again
+        obj = {
+            'count_type': 'total',
+            'name': 'standing_again',
+            'count': standing_again,
+            'object_id': 'standing_again',
         }
         self.add_or_update(obj)
