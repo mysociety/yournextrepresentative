da2c7edb8fa1fe3fb6bf9e0bcd4f0a487a7a9546
Sym Roe <sym.roe@talusdesign.co.uk>
2014-12-16 19:24:43 +0000

    Add ReportsHomeView and nav item

--- cached_counts/urls.py
+++ cached_counts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import PartyCountsView, ConstituencyCountsView
+from .views import PartyCountsView, ConstituencyCountsView, ReportsHomeView
 
 urlpatterns = patterns('',
+    url(r'^$', ReportsHomeView.as_view(), name='reports_home'),
     url(r'parties$', PartyCountsView.as_view(), name='party_counts'),
     url(r'constituencies$', ConstituencyCountsView.as_view(), name='constituencies_counts'),
 )
--- cached_counts/views.py
+++ cached_counts/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from .models import CachedCount
+
+class ReportsHomeView(TemplateView):
+    template_name = "reports.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ReportsHomeView, self).get_context_data(**kwargs)
+        try:
+            context['candidates_2015'] = CachedCount.objects.get(
+                object_id='candidates_2015').count
+            context['percent_of_2010'] = 100 * float(
+                context['candidates_2015'])/float(4152)
+        except CachedCount.DoesNotExist:
+            pass
+        try:
+            context['new_candidates'] = CachedCount.objects.get(
+                object_id='new_candidates').count
+        except CachedCount.DoesNotExist:
+            pass
+        try:
+            context['standing_again'] = CachedCount.objects.get(
+                object_id='standing_again').count
+        except CachedCount.DoesNotExist:
+            pass
+        return context
 
 class PartyCountsView(ListView):
     template_name = "party_counts.html"
