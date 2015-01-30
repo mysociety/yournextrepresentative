3d00b341d4da7491a12b5c5c3fae12a648f03b4c
Mark Longair <mhl@pobox.com>
2015-01-30 13:59:55 +0000

    Temporary fix for a PopIt issue (proxied image links missing)

--- candidates/templatetags/imagefixes.py
+++ candidates/templatetags/imagefixes.py
@@ -8,6 +8,9 @@ register = template.Library()
 
 @register.filter(name='fixproxyurl')
 def fixproxyurl(value):
+    # FIXME: Temporary fix for a PopIt bug, where image_proxy is missing:
+    if not value:
+        return ''
     # Some PopIt versions have a double-slash accidentally included:
     result = re.sub(r'image-proxy//', 'image-proxy/', value)
     if settings.FORCE_HTTPS_IMAGES:
