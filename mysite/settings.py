c62b0cb3bc60514834b4a380689dd429a20ea6d7
Sym Roe <sym.roe@talusdesign.co.uk>
2015-02-03 12:13:17 +0000

    Add MEDIA_ROOT to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -163,6 +163,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
