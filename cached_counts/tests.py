e40a8ce30f574a8e2745fdf2c1a74e4f1c00bc0d
Sym Roe <sym.roe@talusdesign.co.uk>
2015-02-16 10:08:08 +0000

    Create initial counts outside the test class

--- cached_counts/tests.py
+++ cached_counts/tests.py
@@ -6,24 +6,29 @@ from candidates.tests.test_create_person import mock_create_person
 
 from .models import CachedCount
 
+def create_initial_counts(extra=()):
+    initial_counts = (
+        {
+            'count_type': 'constituency',
+            'name': 'Dulwich and West Norwood',
+            'count': 10,
+            'object_id': '65808'
+        },
+        {
+            'count_type': 'party',
+            'name': 'Labour',
+            'count': 0,
+            'object_id': 'party:53'
+        },
+    )
+    initial_counts = initial_counts + extra
+
+    for count in initial_counts:
+        CachedCount(**count).save()
+
 class CachedCountTechCase(TestCase):
     def setUp(self):
-        initial_counts = (
-            {
-                'count_type': 'constituency',
-                'name': 'Dulwich and West Norwood',
-                'count': 10,
-                'object_id': '65808'
-            },
-            {
-                'count_type': 'party',
-                'name': 'Labour',
-                'count': 0,
-                'object_id': 'party:53'
-            },
-        )
-        for count in initial_counts:
-            CachedCount(**count).save()
+        create_initial_counts()
 
     def test_object_urls(self):
         for count in CachedCount.objects.filter(count_type='constituency'):
