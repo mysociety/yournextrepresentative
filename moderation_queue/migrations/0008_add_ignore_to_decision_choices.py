3209a38b795cb5519f92bbfc2651df5b69ba0f76
Mark Longair <mhl@pobox.com>
2015-03-22 12:22:18 +0000

    Add a forgotten migration (to add 'ignore' as a decision choice)
    This should have gone into d5086f1d74d448

---
+++ moderation_queue/migrations/0008_add_ignore_to_decision_choices.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('moderation_queue', '0007_auto_20150303_1420'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='queuedimage',
+            name='decision',
+            field=models.CharField(default=b'undecided', max_length=32, choices=[(b'approved', b'Approved'), (b'rejected', b'Rejected'), (b'undecided', b'Undecided'), (b'ignore', b'Ignore')]),
+            preserve_default=True,
+        ),
+    ]
