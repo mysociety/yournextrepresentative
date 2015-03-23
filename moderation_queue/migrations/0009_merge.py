ca6e43fa10d8f68034d593646e99dd004852b09e
Mark Longair <mhl@pobox.com>
2015-03-23 10:45:08 +0000

    Add a merge migration, dependent on both the 0008 migrations
    ... in the moderation_queue app.

---
+++ moderation_queue/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('moderation_queue', '0008_add_photo_review_permissions'),
+        ('moderation_queue', '0008_add_ignore_to_decision_choices'),
+    ]
+
+    operations = [
+    ]
