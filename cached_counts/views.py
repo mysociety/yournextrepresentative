6253f79ac811c89d9bd6d3553bb720a649281660
Mark Longair <mhl@pobox.com>
2015-03-27 13:22:17 +0000

    Reduce number of queries in ReportsHomeView.get_counts
    This commit also changes get_counts so it always returns a new
    dict, rather than having a side-effect if it's called with the
    context parameter.

--- cached_counts/views.py
+++ cached_counts/views.py
@@ -10,31 +10,29 @@ from .models import CachedCount
 class ReportsHomeView(TemplateView):
     template_name = "reports.html"
 
-    def get_counts(self, context=None):
-        if not context:
-            context = {}
-        for object_id in (
-                'candidates_2015',
-                'candidates_2010',
-                'new_candidates',
-                'standing_again',
-                'standing_again_same_party',
-                'standing_again_different_party',
-        ):
-            try:
-                context[object_id] = CachedCount.objects.get(
-                    object_id=object_id).count
-            except CachedCount.DoesNotExist:
-                pass
-        if context.get('candidates_2010') and 'candidates_2015' in context:
-            context['percent_of_2010'] = \
-                (100 * float(context['candidates_2015'])) / \
-                context['candidates_2010']
-        return context
+    def get_counts(self):
+        count_keys = (
+            'candidates_2015',
+            'candidates_2010',
+            'new_candidates',
+            'standing_again',
+            'standing_again_same_party',
+            'standing_again_different_party',
+        )
+        counts = {
+            d['object_id']: d['count']
+            for d in CachedCount.objects.filter(object_id__in=count_keys). \
+                values('object_id', 'count')
+        }
+        if counts.get('candidates_2010') and 'candidates_2015' in counts:
+            counts['percent_of_2010'] = \
+                (100 * float(counts['candidates_2015'])) / \
+                counts['candidates_2010']
+        return counts
 
     def get_context_data(self, **kwargs):
         context = super(ReportsHomeView, self).get_context_data(**kwargs)
-        context = self.get_counts(context)
+        context.update(self.get_counts())
         return context
 
     def get(self, *args, **kwargs):
