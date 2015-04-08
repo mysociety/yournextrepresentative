5f6338ab9dfb02fb4d2f5ecf498e571c29fac497
Mark Longair <mhl@pobox.com>
2015-04-08 16:22:46 +0100

    In the admin, add search for queued images
    This allows you to search for queued images by either PopIt person ID or
    the uploading user's name.

--- moderation_queue/admin.py
+++ moderation_queue/admin.py
@@ -3,6 +3,7 @@ from .models import QueuedImage
 
 class QueuedImageAdmin(admin.ModelAdmin):
     list_display = ('user', 'popit_person_id', 'created', 'decision')
+    search_fields = ('user__username', 'popit_person_id')
     ordering = ('-created',)
 
 admin.site.register(QueuedImage, QueuedImageAdmin)
