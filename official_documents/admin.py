e93f6599259bcf16a3db7c927c7fc6ce5b6be5b8
Mark Longair <mhl@pobox.com>
2015-04-10 14:27:21 +0100

    Add the OfficialDocument model to the admin

--- official_documents/admin.py
+++ official_documents/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import OfficialDocument
+
+class OfficialDocumentAdmin(admin.ModelAdmin):
+    list_display = ('document_type', 'created', 'mapit_id', 'source_url')
+    search_fields = ('mapit_id', 'source_url')
+    list_filter = ('document_type',)
+    ordering = ('-created',)
+
+admin.site.register(OfficialDocument, OfficialDocumentAdmin)
