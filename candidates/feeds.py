fc2aed82b63cb73f9a5b620ec38c62c25b8ba776
Mark Longair <mhl@pobox.com>
2015-04-10 20:34:35 +0100

    Fix feeds.xml for the constituency update workaround
    This is a temporary measure; really we should just add a constituency
    field to LoggedAction but this is just to stop the error emails for the
    moment.

--- candidates/feeds.py
+++ candidates/feeds.py
@@ -1,8 +1,13 @@
+import re
+
 from django.contrib.syndication.views import Feed
 from django.core.urlresolvers import reverse
 from django.utils.feedgenerator import Atom1Feed
+from django.utils.text import slugify
 
 from .models import LoggedAction
+
+lock_re = re.compile(r'^(?:Unl|L)ocked\s*constituency (.*) \((\d+)\)$')
 
 class RecentChangesFeed(Feed):
     title = "YourNextMP recent changes"
@@ -14,9 +19,16 @@ class RecentChangesFeed(Feed):
         return LoggedAction.objects.order_by('-updated')[:50]
 
     def item_title(self, item):
-        return u"{0} - {1}".format(
-            item.popit_person_id,
-            item.action_type
+        m = lock_re.search(item.source)
+        if m:
+            return u"{0} - {1}".format(
+                m.group(1),
+                item.action_type
+            )
+        else:
+            return u"{0} - {1}".format(
+                item.popit_person_id,
+                item.action_type
             )
 
     def item_description(self, item):
@@ -32,4 +44,16 @@ class RecentChangesFeed(Feed):
         return description
 
     def item_link(self, item):
-        return reverse('person-view', args=[item.popit_person_id])
+        # As a hack for the moment, constituencies are just mentioned
+        # in the source message:
+        m = lock_re.search(item.source)
+        if m:
+            return reverse('constituency', kwargs={
+                'mapit_area_id': m.group(2),
+                'ignored_slug': slugify(m.group(1))
+            })
+        else:
+            if item.popit_person_id:
+                return reverse('person-view', args=[item.popit_person_id])
+            else:
+                return '/'
