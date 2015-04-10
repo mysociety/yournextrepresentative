mysociety/yournextrepresentative

commit af1e8cfe463cf267361004f8b376f574e51fd97c

Make sure all management commands invalidate cache data properly

candidates/management/commands/candidates_update_popit_parties_from_ec_data.py
@@ -14,6 +14,7 @@ import requests
 from slumber.exceptions import HttpServerError
 import dateutil.parser
 
+from candidates.models import invalidate_cache_entries_from_person_data
 from candidates.popit import create_popit_api_object
 
 from ..images import get_file_md5sum, image_uploaded_already
@@ -110,6 +111,21 @@ class Command(BaseCommand):
                         self.upload_images(ec_party['emblems'], party_id)
                     else:
                         raise
+                organization_with_memberships = \
+                    self.api.organizations(
+                        party_id,
+                        embed='membership.person'
+                    )['result']
+                # Make sure any members of these parties are
+                # invalidated from the cache so that the embedded
+                # party information when getting posts and persons is
+                # up-to-date:
+                for membership in organization_with_memberships.get(
+                        'memberships', []
+                ):
+                    invalidate_cache_entries_from_person_data(
+                        membership['person_id']
+                    )
 
     def clean_date(self, date):
         return dateutil.parser.parse(date).strftime("%Y-%m-%d")

candidates/management/commands/candidates_add_identifier.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
+from candidates.models import invalidate_cache_entries_from_person_data
 from candidates.popit import create_popit_api_object
 from candidates.update import PersonParseMixin, PersonUpdateMixin
 from candidates.views import CandidacyMixin
@@ -35,6 +36,8 @@ class Command(PersonParseMixin, PersonUpdateMixin, CandidacyMixin, BaseCommand):
             popit.persons(person_id).put(person_data)
         except (HttpClientError, HttpServerError):
             raise CommandError("Failed to update that person")
+
+        invalidate_cache_entries_from_person_data(person_data)
 
         # FIXME: this should create a new version in the versions
         # array too, otherwise you manually have to edit on YourNextMP

candidates/management/commands/candidates_add_other_name.py
@@ -2,6 +2,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
 
+from candidates.models import invalidate_cache_entries_from_person_data
 from candidates.popit import create_popit_api_object
 from candidates.update import PersonParseMixin, PersonUpdateMixin
 from candidates.views import CandidacyMixin
@@ -45,6 +46,8 @@ class Command(PersonParseMixin, PersonUpdateMixin, CandidacyMixin, BaseCommand):
             popit.persons(person_id).put(person_data)
         except (HttpClientError, HttpServerError):
             raise CommandError("Failed to update that person")
+
+        invalidate_cache_entries_from_person_data(person_data)
 
         # FIXME: this should create a new version in the versions
         # array too, otherwise you manually have to edit on YourNextMP

candidates/management/commands/candidates_add_parlparse_id.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 import json
 from lxml import objectify
 
+from candidates.models import invalidate_cache_entries_from_person_data
 from candidates.popit import create_popit_api_object
 from candidates.update import PersonParseMixin, PersonUpdateMixin
 from candidates.views.version_data import get_change_metadata
@@ -102,6 +103,7 @@ class Command(PersonParseMixin, PersonUpdateMixin, BaseCommand):
             change_metadata,
             previous_versions,
         )
+        invalidate_cache_entries_from_person_data(person_data)
         if self.verbosity > 0:
             msg = u"Successfully updated {} with {}"
             self.stdout.write(msg.format(name, parlparse_id))

candidates/management/commands/candidates_delete_party_images.py
@@ -1,3 +1,4 @@
+from candidates.models import invalidate_cache_entries_from_person_data
 from candidates.popit import PopItApiMixin, popit_unwrap_pagination
 
 from django.core.management.base import BaseCommand
@@ -7,7 +8,8 @@ class Command(PopItApiMixin, BaseCommand):
     def handle(self, **options):
         for o in popit_unwrap_pagination(
                 self.api.organizations,
-                per_page=100
+                per_page=100,
+                embed='membership.person'
         ):
             if o['classification'] != 'Party':
                 continue
@@ -15,3 +17,8 @@ class Command(PopItApiMixin, BaseCommand):
             for image in o.get('images', []):
                 print "  DELETE", image['_id']
                 self.api.organizations(o['id']).image(image['_id']).delete()
+            # The person pages get party images via the
+            # membership.organization embed, so invalidate the cache
+            # entries for any person who's a member of this party:
+            for membership in o.get('memberships', []):
+                invalidate_cache_entries_from_person_data(membership['person_id'])

candidates/management/commands/candidates_fix_image_metadata.py
@@ -5,6 +5,7 @@ import requests
 import sys
 from StringIO import StringIO
 
+from candidates.models import invalidate_cache_entries_from_person_data
 from candidates.popit import PopItApiMixin, popit_unwrap_pagination
 from candidates.update import fix_dates
 
@@ -100,3 +101,4 @@ class Command(PopItApiMixin, BaseCommand):
             except HttpClientError as e:
                 print "HttpClientError", e.content
                 sys.exit(1)
+            invalidate_cache_entries_from_person_data(person)

candidates/management/commands/candidates_import_ppcs.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 import requests
 
+from candidates.models import invalidate_cache_entries_from_person_data
 from candidates.update import PersonParseMixin, PersonUpdateMixin
 from candidates.static_data import MapItData
 from candidates.views.version_data import get_change_metadata
@@ -282,6 +283,7 @@ class Command(PersonParseMixin, PersonUpdateMixin, BaseCommand):
             else:
                 print "Uploading image..."
                 self.upload_person_image(person_id, image_filename, ppc_data['image_url'])
+        invalidate_cache_entries_from_person_data(merged_person_data)
         return person_id
 
     def add_popit_person(self, ppc_data, image_filename):
@@ -293,6 +295,7 @@ class Command(PersonParseMixin, PersonUpdateMixin, BaseCommand):
         person_id = self.create_person(person_data, change_metadata)
         if image_filename:
             self.upload_person_image(person_id, image_filename, ppc_data['image_url'])
+        invalidate_cache_entries_from_person_data(person_data)
         return person_id
 
     def already_added_for_2015(self, ppc_data, popit_result):

candidates/management/commands/candidates_recreate_memberships.py
@@ -6,6 +6,7 @@
 
 from django.core.management.base import BaseCommand, CommandError
 
+from candidates.cache import invalidate_posts, invalidate_person
 from candidates.models import PopItPerson
 from candidates.popit import create_popit_api_object
 from candidates.update import PersonUpdateMixin
@@ -22,6 +23,10 @@ class Command(PersonUpdateMixin, BaseCommand):
             person = PopItPerson.create_from_popit(
                 create_popit_api_object(), person_id
             )
+            posts_to_invalidate = person.get_associated_posts()
             person.delete_memberships()
             self.create_party_memberships(person_id, person.popit_data)
             self.create_candidate_list_memberships(person_id, person.popit_data)
+            posts_to_invalidate.update(person.get_associated_posts())
+            invalidate_posts(posts_to_invalidate)
+            invalidate_person(person_id)

commit 98e14bea46d721e81131172688a4173022541ddb

Fix the candidates_update_popit_parties_from_ec_data command
This fixes a bug introduced in af1e8cfe463cf2673

candidates/management/commands/candidates_update_popit_parties_from_ec_data.py
@@ -112,10 +112,7 @@ class Command(BaseCommand):
                     else:
                         raise
                 organization_with_memberships = \
-                    self.api.organizations(
-                        party_id,
-                        embed='membership.person'
-                    )['result']
+                    self.api.organizations(party_id).get(embed='membership.person')['result']
                 # Make sure any members of these parties are
                 # invalidated from the cache so that the embedded
                 # party information when getting posts and persons is