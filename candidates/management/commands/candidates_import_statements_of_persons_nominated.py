dd67154dee00a0a97f398cb2536e558652de13bf
Mark Longair <mhl@pobox.com>
2015-04-10 12:47:15 +0100

    Add a script to import Statements of Persons Nominated
    This downloads the Google Spreadsheet of our crowd-sourced Statements of
    Persons Nominated.

---
+++ candidates/management/commands/candidates_import_statements_of_persons_nominated.py
@@ -0,0 +1,91 @@
+import csv
+import errno
+import hashlib
+import magic
+import mimetypes
+import os
+from os.path import dirname, join, exists
+import requests
+
+from django.core.management.base import BaseCommand
+from django.core.files.storage import FileSystemStorage 
+
+from candidates.static_data import MapItData
+from official_documents.models import OfficialDocument
+
+CSV_URL = 'https://docs.google.com/a/mysociety.org/spreadsheets/d/1jvWaQSENnASZfGne1IWRbDATMH2NT2xutyPEbZ5Is-8/export?format=csv&id=1jvWaQSENnASZfGne1IWRbDATMH2NT2xutyPEbZ5Is-8&gid=0'
+
+allowed_mime_types = set([
+    'application/pdf',
+    'application/msword',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+])
+
+def download_file_cached(url):
+    url_hash = hashlib.md5(url).hexdigest()
+    directory = join(dirname(__file__), '.noms-cache')
+    try:
+        os.mkdir(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    filename = join(directory, url_hash)
+    if exists(filename):
+        return filename
+    r = requests.get(url)
+    with open(filename, 'w') as f:
+        f.write(r.content)
+    return filename
+
+class Command(BaseCommand):
+
+    def handle(self, **options):
+        
+        mime_type_magic = magic.Magic(mime=True)
+        storage = FileSystemStorage()
+
+        r = requests.get(CSV_URL, stream=True)
+        reader = csv.DictReader(r.raw)
+        for row in reader:
+            name = row['Constituency'].decode('utf-8')
+            cons_data = MapItData.constituencies_2010_name_map[name]
+            document_url = row['URL']
+            if not document_url:
+                print u"No URL for {0}".format(name)
+                continue
+            existing_documents = OfficialDocument.objects.filter(
+                mapit_id=cons_data['id']
+            )
+            if existing_documents.count() > 0:
+                print u"Skipping {0} since it already had documents".format(name)
+                continue
+            try:
+                downloaded_filename = download_file_cached(document_url)
+            except requests.exceptions.SSLError:
+                print u"Skipping {0} due to a certificate error".format(name)
+                print u"The URL was:", document_url
+                continue
+            except requests.exceptions.ConnectionError:
+                print u"Connection failed for {0}".format(name)
+                print u"The URL was:", document_url
+                continue
+            mime_type = mime_type_magic.from_file(downloaded_filename)
+            extension = mimetypes.guess_extension(mime_type)
+            if mime_type not in allowed_mime_types:
+                print "Ignoring unknown MIME type {0} for {1}".format(
+                    mime_type,
+                    name,
+                )
+                continue
+            filename = "official_documents/{mapit_id}/statement-of-persons-nominated{extension}".format(
+                mapit_id=cons_data['id'],
+                extension=extension,
+            )
+            with open(downloaded_filename, 'rb') as f:
+                storage_filename = storage.save(filename, f)
+            OfficialDocument.objects.create(
+                document_type=OfficialDocument.NOMINATION_PAPER,
+                uploaded_file=storage_filename,
+                mapit_id=cons_data['id'],
+                source_url=document_url
+            )
