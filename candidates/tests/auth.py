3058b68da97b37520c8ead16b571fac4c42bb3fa
Mark Longair <mhl@pobox.com>
2015-05-01 15:01:33 +0100

    Refactor test user creation

--- candidates/tests/auth.py
+++ candidates/tests/auth.py
@@ -12,67 +12,38 @@ class TestUserMixin(object):
 
     @classmethod
     def setUpClass(cls):
-        cls.user = User.objects.create_user(
-            'john',
-            'john@example.com',
-            'notagoodpassword',
-        )
-        cls.user_who_can_merge = User.objects.create_user(
-            'alice',
-            'alice@example.com',
-            'alsonotagoodpassword',
-        )
-        cls.user_who_can_lock = User.objects.create_user(
-            'charles',
-            'charles@example.com',
-            'alsonotagoodpassword',
-        )
-        cls.user_who_can_upload_documents = User.objects.create_user(
-            'delilah',
-            'delilah@example.com',
-            'alsonotagoodpassword',
-        )
-        cls.user_who_can_rename = User.objects.create_user(
-            'ermintrude',
-            'ermintrude@example.com',
-            'stillreallynotagoodpassword',
-        )
-        cls.user_who_can_record_results = User.objects.create_user(
-            'frankie',
-            'frankie@example.com',
-            'stillreallynotagoodpassword',
-        )
-        merger_group = Group.objects.get(name=TRUSTED_TO_MERGE_GROUP_NAME)
-        merger_group.user_set.add(cls.user_who_can_merge)
-        locker_group = Group.objects.get(name=TRUSTED_TO_LOCK_GROUP_NAME)
-        locker_group.user_set.add(cls.user_who_can_lock)
-        uploader_group = Group.objects.get(name=DOCUMENT_UPLOADERS_GROUP_NAME)
-        uploader_group.user_set.add(cls.user_who_can_upload_documents)
-        renamer_group = Group.objects.get(name=TRUSTED_TO_RENAME_GROUP_NAME)
-        renamer_group.user_set.add(cls.user_who_can_rename)
-        recorder_group = Group.objects.get(name=RESULT_RECORDERS_GROUP_NAME)
-        recorder_group.user_set.add(cls.user_who_can_record_results)
-        for u in (
-                cls.user,
-                cls.user_who_can_merge,
-                cls.user_who_can_lock,
-                cls.user_who_can_upload_documents,
-                cls.user_who_can_rename
+        cls.users_to_delete = []
+        for username, attr, group_names in (
+                ('john', 'user', []),
+                ('alice', 'user_who_can_merge', [TRUSTED_TO_MERGE_GROUP_NAME]),
+                ('charles', 'user_who_can_lock', [TRUSTED_TO_LOCK_GROUP_NAME]),
+                ('delilah', 'user_who_can_upload_documents', [DOCUMENT_UPLOADERS_GROUP_NAME]),
+                ('ermintrude', 'user_who_can_rename', [TRUSTED_TO_RENAME_GROUP_NAME]),
+                ('frankie', 'user_who_can_record_results', [RESULT_RECORDERS_GROUP_NAME]),
         ):
+            u = User.objects.create_user(
+                username,
+                username + '@example.com',
+                'notagoodpassword',
+            )
             terms = u.terms_agreement
             terms.assigned_to_dc = True
             terms.save()
+            for group_name in group_names:
+                group = Group.objects.get(name=group_name)
+                group.user_set.add(u)
+            setattr(cls, attr, u)
+            cls.users_to_delete.append(u)
+        # Also add a user who hasn't accepted the terms, and isn't in
+        # any groups:
         cls.user_refused = User.objects.create_user(
             'johnrefused',
             'johnrefused@example.com',
             'notagoodpasswordeither',
         )
+        cls.users_to_delete.append(cls.user_refused)
 
     @classmethod
     def tearDownClass(cls):
-        cls.user_refused.delete()
-        cls.user_who_can_rename.delete()
-        cls.user_who_can_upload_documents.delete()
-        cls.user_who_can_lock.delete()
-        cls.user_who_can_merge.delete()
-        cls.user.delete()
+        for u in cls.users_to_delete:
+            u.delete()
