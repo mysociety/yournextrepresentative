86e07a512d097cc026abd3d3dd781ff9f68e7e16
Mark Longair <mhl@pobox.com>
2015-05-02 17:29:08 +0100

    Add search and filtering to the 'Logged action' index in the admin

--- candidates/admin.py
+++ candidates/admin.py
@@ -11,12 +11,17 @@ class LoggedActionAdminForm(ModelForm):
 
 class LoggedActionAdmin(admin.ModelAdmin):
     form = LoggedActionAdminForm
+    search_fields = ('user__username', 'popit_person_new_version',
+                     'popit_person_id', 'ip_address', 'source')
+    list_filter = ('action_type',)
     list_display = ['user', 'ip_address', 'action_type',
                     'popit_person_new_version', 'popit_person_link',
                     'created', 'updated', 'source']
     ordering = ('-created',)
 
     def popit_person_link(self, o):
+        if not o.popit_person_id:
+            return ''
         url = reverse('person-view', kwargs={'person_id': o.popit_person_id})
         return '<a href="{0}">{1}</a>'.format(
             url,
