e990fb2b0b3ed61250313cdb3ec3604f6c49e3c1
Mark Longair <mhl@pobox.com>
2015-05-11 18:50:42 +0100

    Update the candidacy create / delete view to use PopItPerson
    This really should have been done in fa7ca4d5bd61f469e but was somehow
    missed.

--- candidates/views/candidacies.py
+++ candidates/views/candidacies.py
@@ -10,7 +10,7 @@ from .helpers import get_redirect_from_mapit_id
 from .mixins import CandidacyMixin
 from .version_data import get_client_ip, get_change_metadata
 from ..forms import CandidacyCreateForm, CandidacyDeleteForm
-from ..models import LoggedAction, TRUSTED_TO_LOCK_GROUP_NAME
+from ..models import PopItPerson, LoggedAction, TRUSTED_TO_LOCK_GROUP_NAME
 from ..popit import PopItApiMixin
 from ..static_data import MapItData
 
@@ -25,7 +25,7 @@ def raise_if_locked(api, request, post_id):
         raise Exception("Attempt to edit a candidacy in a locked constituency")
 
 
-class CandidacyView(LoginRequiredMixin, CandidacyMixin, PopItApiMixin, FormView):
+class CandidacyView(LoginRequiredMixin, PopItApiMixin, FormView):
 
     form_class = CandidacyCreateForm
     template_name = 'candidates/candidacy-create.html'
@@ -36,24 +36,27 @@ class CandidacyView(LoginRequiredMixin, CandidacyMixin, PopItApiMixin, FormView)
         change_metadata = get_change_metadata(
             self.request, form.cleaned_data['source']
         )
+        person = PopItPerson.create_from_popit(
+            self.api,
+            form.cleaned_data['person_id'],
+        )
         LoggedAction.objects.create(
             user=self.request.user,
             action_type='candidacy-create',
             ip_address=get_client_ip(self.request),
             popit_person_new_version=change_metadata['version_id'],
-            popit_person_id=form.cleaned_data['person_id'],
+            popit_person_id=person.id,
             source=change_metadata['information_source'],
         )
-        our_person, _ = self.get_person(form.cleaned_data['person_id'])
-        previous_versions = our_person.pop('versions')
-
-        our_person['standing_in']['2015'] = get_area_from_post_id(
+        person.standing_in['2015'] = get_area_from_post_id(
             mapit_area_id,
             mapit_url_key='mapit_url'
         )
-        our_person['party_memberships']['2015'] = our_person['party_memberships']['2010']
+        person.party_memberships['2015'] = person.party_memberships['2010']
 
-        self.update_person(our_person, change_metadata, previous_versions)
+        person.record_version(change_metadata)
+        person.save_to_popit(self.api)
+        person.invalidate_cache_entries()
         return get_redirect_from_mapit_id(mapit_area_id)
 
     def get_context_data(self, **kwargs):
@@ -65,7 +68,7 @@ class CandidacyView(LoginRequiredMixin, CandidacyMixin, PopItApiMixin, FormView)
         return context
 
 
-class CandidacyDeleteView(LoginRequiredMixin, CandidacyMixin, PopItApiMixin, FormView):
+class CandidacyDeleteView(LoginRequiredMixin, PopItApiMixin, FormView):
 
     form_class = CandidacyDeleteForm
     template_name = 'candidates/candidacy-delete.html'
@@ -76,21 +79,25 @@ class CandidacyDeleteView(LoginRequiredMixin, CandidacyMixin, PopItApiMixin, For
         change_metadata = get_change_metadata(
             self.request, form.cleaned_data['source']
         )
+        person = PopItPerson.create_from_popit(
+            self.api,
+            form.cleaned_data['person_id'],
+        )
         LoggedAction.objects.create(
             user=self.request.user,
             action_type='candidacy-delete',
             ip_address=get_client_ip(self.request),
             popit_person_new_version=change_metadata['version_id'],
-            popit_person_id=form.cleaned_data['person_id'],
+            popit_person_id=person.id,
             source=change_metadata['information_source'],
         )
-        our_person, _ = self.get_person(form.cleaned_data['person_id'])
-        previous_versions = our_person.pop('versions')
 
-        our_person['standing_in']['2015'] = None
-        our_person['party_memberships'].pop('2015', None)
+        person.standing_in['2015'] = None
+        person.party_memberships.pop('2015', None)
 
-        self.update_person(our_person, change_metadata, previous_versions)
+        person.record_version(change_metadata)
+        person.save_to_popit(self.api)
+        person.invalidate_cache_entries()
         return get_redirect_from_mapit_id(mapit_area_id)
 
     def get_context_data(self, **kwargs):
