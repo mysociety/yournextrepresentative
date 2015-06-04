0eea4c7af5c1702ac5196a65fd426398629b1fc3
Mark Longair <mhl@pobox.com>
2015-06-04 21:01:12 +0100

    Add an extension to DjDT for timing templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'moderation_queue',
     'auth_helpers',
     'debug_toolbar',
+    'template_timings_panel',
     'official_documents',
     'results',
 )
@@ -161,6 +162,22 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+]
 
 INTERNAL_IPS = ['127.0.0.1']
 
