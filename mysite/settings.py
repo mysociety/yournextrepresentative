e80c0286b3d5ac96b56cfcf061dbb9f22c3675af
Matthew Somerville <matthew-github@dracos.co.uk>
2015-06-18 15:48:34 +0100

    Allow default language/timezone to be configured.

--- mysite/settings.py
+++ mysite/settings.py
@@ -208,9 +208,9 @@ else:
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-gb'
+LANGUAGE_CODE = conf.get('LANGUAGE_CODE', 'en-gb')
 
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = conf.get('TIME_ZONE', 'Europe/London')
 
 USE_I18N = True
 
