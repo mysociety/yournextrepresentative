176255cf0613301021f9e745440670ddd07d2b42
Matthew Somerville <matthew-github@dracos.co.uk>
2015-06-18 15:43:11 +0100

    Add initial es_AR file, and locale middleware.

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,7 @@ MIDDLEWARE_CLASSES = (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'candidates.middleware.PopItDownMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
