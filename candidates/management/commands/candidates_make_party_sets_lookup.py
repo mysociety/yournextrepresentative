29b23609e75c90c28e46aea23bf7461e78f27c44
Mark Longair <mhl@pobox.com>
2015-06-19 15:21:36 +0100

    Update candidates_make_party_sets_lookup to use AREA_POST_DATA

--- candidates/management/commands/candidates_make_party_sets_lookup.py
+++ candidates/management/commands/candidates_make_party_sets_lookup.py
@@ -4,7 +4,7 @@ from os.path import dirname, join, realpath
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from candidates.election_specific import post_id_to_party_set
+from candidates.election_specific import AREA_POST_DATA
 from candidates.popit import get_all_posts
 
 class Command(BaseCommand):
@@ -22,9 +22,16 @@ class Command(BaseCommand):
         with open(output_filename, 'w') as f:
             f.write('var postIDToPartySet = ')
             mapping = {
-                post['id']: post_id_to_party_set(post['id'])
+                post['id']: AREA_POST_DATA.post_id_to_party_set(post['id'])
                 for election, election_data in settings.ELECTIONS_CURRENT
                 for post in get_all_posts(election_data['for_post_role'])
             }
+            unknown_post_ids = [
+                k for k, v in mapping.items()
+                if v is None
+            ]
             f.write(json.dumps(mapping))
             f.write(';\n')
+            if unknown_post_ids:
+                print "Warning: no party set could be found for these post IDs:"
+                print unknown_post_ids
