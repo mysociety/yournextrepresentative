mysociety/yournextrepresentative

commit d948b8fd853118efe68dc63e6b9ec2a545ee6de7

Restructure the loading of (possibly country-specific) static data
This commit addresses two problems:

 * The code for mapping to party sets and post groups was
   election_specific but there was no natural way to implement this
   custom code, except for adding more and more functions to
   election_specific.py, which is messy.

 * Those election_specific.py functions were interdependent with the
   static_data.py data but defined on a different import path which
   meant it was easy to create knotty circular dependency errors.

This commit changes the MapItData and PartyData classes from essentially
just namespaces to classes that are intended to be instantiated in order
to get the data.  They should be subclassed by each election in order to
provide custom behaviour.

This also adds an AreaPostData for any data mappings that might rely on
both MapIt and Party data.

Hopefully this will be easier to understand and deal with.

candidates/static_data.py
@@ -7,17 +7,9 @@ import requests
 from django.conf import settings
 from django.utils.translation import ugettext as _
 
-from candidates.election_specific import area_to_post_group
-
 data_directory = abspath(join(
     dirname(__file__), '..', 'elections', settings.ELECTION_APP, 'data'
 ))
-
-MAPIT_TYPES_GENERATIONS_ELECTIONS = {
-    (mapit_type, t[1]['mapit_generation']): t[1]
-    for t in settings.ELECTIONS_CURRENT
-    for mapit_type in t[1]['mapit_types']
-}
 
 def get_mapit_areas(area_type, generation):
     expected_filename = join(
@@ -44,73 +36,152 @@ def get_mapit_areas(area_type, generation):
         r = requests.get(mapit_url)
         return r.json()
 
-# What do we need to do with MapIt data?
-#
-#  Just area related:
-#
-#    Go from MapIt ID to full area data (currently areas_by_id, was constituencies_2010)
-#    Go from an area name and type to full area data (currently areas_by_name, was constituencies_2010_name_map)
-#    Get a list of all areas of a particular type (areas_list_sorted_by_name, was constituencies_2010_name_sorted)
-#    Get a list of all areas of a particular type in a given "post group" (currently area_ids_and_names_by_post_group, was constituencies_2010_by_post_group)
-#
-#  Related to posts too:
-#
-#    Go from a MapIt ID to the posts are associated with it:
-#       (probably need a new class method for that...)
 
-class MapItData(object):
+class BaseMapItData(object):
 
-    areas_by_id = {}
-    areas_by_name = {}
-    areas_list_sorted_by_name = {}
-    area_ids_and_names_by_post_group = {}
-    areas_by_post_id = {}
+    """Load MapIt data and make it availale in helpful data structures
 
-    for t, election_data in MAPIT_TYPES_GENERATIONS_ELECTIONS.items():
-        mapit_type, mapit_generation = t
-        areas_by_id[t] = get_mapit_areas(mapit_type, mapit_generation)
-        for area in areas_by_id[t].values():
-            areas_by_name.setdefault(t, {})
-            areas_by_name[t][area['name']] = area
-            post_id = election_data['get_post_id'](mapit_type, area['id'])
-            if post_id in areas_by_post_id:
-                message = _("Found multiple areas for the post ID {post_id}")
-                raise Exception(message.format(post_id=post_id))
-            areas_by_post_id[post_id] = area
-        areas_list_sorted_by_name[t] = sorted(
-            areas_by_id[t].items(),
-            key=lambda c: c[1]['name']
-        )
-        for area in areas_by_name[t].values():
-            post_group = area_to_post_group(area)
-            area_ids_and_names_by_post_group.setdefault(t, defaultdict(list))
-            area_ids_and_names_by_post_group[t][post_group].append(
-                (str(area['id']), area['name'])
+    FIXME: check that these are sensible descriptions
+
+    On instantiation, the following attributes are created:
+
+        'areas_by_id', a dictionary that maps from a MapIt ID to full
+        area data
+
+        'areas_by_name', a dictionary that maps an area name and typ
+        to full area data
+
+        'areas_list_sorted_by_name', a list of all areas of a
+        particular type
+
+    """
+
+    def __init__(self):
+
+        self.areas_by_id = {}
+        self.areas_by_name = {}
+        self.areas_list_sorted_by_name = {}
+        self.areas_by_post_id = {}
+
+        for t, election_data in settings.MAPIT_TYPES_GENERATIONS_ELECTIONS.items():
+            mapit_type, mapit_generation = t
+            self.areas_by_id[t] = get_mapit_areas(mapit_type, mapit_generation)
+            for area in self.areas_by_id[t].values():
+                self.areas_by_name.setdefault(t, {})
+                self.areas_by_name[t][area['name']] = area
+            self.areas_list_sorted_by_name[t] = sorted(
+                self.areas_by_id[t].items(),
+                key=lambda c: c[1]['name']
             )
-        for area_list in area_ids_and_names_by_post_group[t].values():
-            area_list.sort(key=lambda c: c[1])
 
-def get_all_parties():
-    result_list = defaultdict(list)
-    result_dict = {}
-    with open(join(data_directory, 'all-parties-from-popit.json')) as f:
-        for party in json.load(f):
-            key = party.get('register', '')
-            result_list[key].append((party['id'], party['name']))
-            result_dict[party['id']] = party['name']
-        # The parties without a register (e.g. the pseudo parties
-        # "Independent" and "Speaker seeking re-election") don't have
-        # a register, but should be added to both the Great Britain
-        # and Northern Ireland lists:
-        for register in ('Great Britain', 'Northern Ireland'):
-            result_list[register] += result_list['']
-        # Then remove those parties without a register:
-        result_list.pop('', None)
-        # Now sort the parties, and an an empty default at the start:
-        for parties in result_list.values():
-            parties.sort(key=lambda p: p[1].lower())
-            parties.insert(0, ('party:none', ''))
-    return result_list, result_dict
 
-class PartyData(object):
-    party_choices, party_id_to_name = get_all_parties()
+class BasePartyData(object):
+
+    """You should subclass this in your election application to define the
+    election-specific 'party sets'.
+
+    FIXME: check that these are sensible descriptions
+
+    When instantiated, this gives you helpful access to party data in
+    two attributes:
+
+    'party_choices' is a dictionary where the keys are the names of
+    party sets and the values are lists that can be used as choices in
+    a ChoiceField, e.g.:
+
+        {u'Great Britain': [
+          ('party:none', ''),
+          (u'party:52', u'Conservative Party'),
+          (u'joint-party:53-119', u'Labour and Co-operative Party'),
+          (u'party:53', u'Labour Party'),
+          (u'ynmp-party:2', u'Independent'),
+          (u'ynmp-party:12522', u'Speaker seeking re-election')],
+         u'Northern Ireland': [
+          ('party:none', ''),
+          (u'party:51', u'Conservative and Unionist Party'),
+          (u'party:434', u'Labour Party of Northern Ireland'),
+          (u'ynmp-party:2', u'Independent'),
+          (u'ynmp-party:12522', u'Speaker seeking re-election')]}
+
+    'party_id_to_name' is a dictionary that maps party IDs to party
+    names, e.g.:
+
+        {u'party:434': u'Labour Party of Northern Ireland',
+         u'party:53': u'Labour Party',
+         u'joint-party:53-119': u'Labour and Co-operative Party',
+         u'party:51': u'Conservative and Unionist Party',
+         u'party:52': u'Conservative Party',
+         u'ynmp-party:12522': u'Speaker seeking re-election',
+         u'ynmp-party:2': u'Independent'}
+
+    """
+
+    def party_data_to_party_set(self, party_data):
+        raise NotImplementedError(
+            "You should implement party_data_to_party_set in a subclass"
+        )
+
+    def __init__(self):
+
+        self.party_choices = defaultdict(list)
+        self.party_id_to_name = {}
+        with open(join(data_directory, 'all-parties-from-popit.json')) as f:
+            for party in json.load(f):
+                for party_set in self.party_data_to_party_sets(party):
+                    self.party_choices[party_set].append(
+                        (party['id'], party['name'])
+                    )
+                    self.party_id_to_name[party['id']] = party['name']
+            # Now sort the parties, and an an empty default at the start:
+            for parties in self.party_choices.values():
+                parties.sort(key=lambda p: p[1].lower())
+                parties.insert(0, ('party:none', ''))
+
+
+class BaseAreaPostData(object):
+
+    """Instantiate this class to provide mappings between areas and posts
+
+    FIXME: check that these are sensible descriptions
+
+    If you instantiate this class you will get the following attributes:
+
+         'area_ids_and_names_by_post_group', maps a post group to a
+         list of all areas of a particular type
+
+         'areas_by_post_id', maps a post ID to all areas associated
+         with it
+    """
+
+    def area_to_post_group(self, area_data):
+        raise NotImplementedError(
+            "You should implement area_to_post_group in a subclass"
+        )
+
+    def get_post_id(mapit_type, area_id):
+        raise NotImplementedError(
+            "You should implement get_post_id in a subclass"
+        )
+
+    def __init__(self, mapit_data, party_data):
+        self.mapit_data = mapit_data
+        self.party_data = party_data
+        self.areas_by_post_id = {}
+        self.area_ids_and_names_by_post_group = {}
+
+        for t, election_data in settings.MAPIT_TYPES_GENERATIONS_ELECTIONS.items():
+            mapit_type, mapit_generation = t
+            for area in self.mapit_data.areas_by_id[t].values():
+                post_id = self.get_post_id(mapit_type, area['id'])
+                if post_id in self.areas_by_post_id:
+                    message = _("Found multiple areas for the post ID {post_id}")
+                    raise Exception(message.format(post_id=post_id))
+                self.areas_by_post_id[post_id] = area
+            for area in mapit_data.areas_by_name[t].values():
+                post_group = self.area_to_post_group(area)
+                self.area_ids_and_names_by_post_group.setdefault(t, defaultdict(list))
+                self.area_ids_and_names_by_post_group[t][post_group].append(
+                    (str(area['id']), area['name'])
+                )
+            for area_list in self.area_ids_and_names_by_post_group[t].values():
+                area_list.sort(key=lambda c: c[1])

elections/uk_general_election_2015/lib.py
@@ -1,31 +1,72 @@
-ALL_POSSIBLE_PARTY_POST_GROUPS = [
-    'England', 'Northern Ireland', 'Scotland', 'Wales'
-]
+import re
 
-def party_to_possible_post_groups(party_data):
-    register = party_data.get('register')
-    if register == 'Northern Ireland':
-        return ('Northern Ireland',)
-    elif register == 'Great Britain':
-        return ('England', 'Scotland', 'Wales')
-    else:
-        return ('England', 'Northern Ireland', 'Scotland', 'Wales')
-
-def area_to_post_group(area_data):
-    return area_data['country_name']
-
-party_sets = (
-    {'slug': 'gb', 'name': 'Great Britain'},
-    {'slug': 'ni', 'name': 'Northern Ireland'},
+from candidates.static_data import (
+    BaseMapItData, BasePartyData, BaseAreaPostData
 )
 
-def post_id_to_party_set(post_id):
-    # Avoid a circular import dependency; it would be better to
-    # restructured the imports to avoid this, so this is just a
-    # workaround (FIXME)
-    from candidates.static_data import MapItData
-    area = MapItData.areas_by_post_id[post_id]
-    if area['country_name'] == 'Northern Ireland':
-        return 'ni'
-    else:
-        return 'gb'
+class MapItData(BaseMapItData):
+    pass
+
+
+class PartyData(BasePartyData):
+
+    def __init__(self):
+        super(PartyData, self).__init__()
+        self.ALL_PARTY_SETS = (
+            {'slug': 'gb', 'name': 'Great Britain'},
+            {'slug': 'ni', 'name': 'Northern Ireland'},
+        )
+
+    def party_data_to_party_sets(self, party_data):
+        register = {
+            'Great Britain': 'gb',
+            'Northern Ireland': 'ni',
+        }.get(party_data.get('register'))
+        if register:
+            return [register]
+        else:
+            # There are some pseudo-parties, like 'Independent' or
+            # 'Speaker seeking re-election' which won't have a register,
+            # so we add them to all party sets.
+            return ['gb', 'ni']
+
+
+class AreaPostData(BaseAreaPostData):
+
+    def __init__(self, *args, **kwargs):
+        super(AreaPostData, self).__init__(*args, **kwargs)
+        self.ALL_POSSIBLE_POST_GROUPS = [
+            'England', 'Northern Ireland', 'Scotland', 'Wales'
+        ]
+
+    def area_to_post_group(self, area_data):
+        return area_data['country_name']
+
+    def get_post_id(self, mapit_type, area_id):
+        return str(area_id)
+
+    def post_id_to_party_set(self, post_id):
+        area = self.mapit_data.areas_by_post_id[post_id]
+        if area['country_name'] == 'Northern Ireland':
+            return 'ni'
+        else:
+            return 'gb'
+
+    def post_id_to_post_group(self, election, post_id):
+        # In the UK, the post IDs are the same as MapIt IDs,
+        # so just look it up from the MapIt data:
+        areas_by_id = self.mapit_data.areas_by_id[('WMC', 22)]
+        area = areas_by_id.get(post_id)
+        return area['country_name']
+
+    def shorten_post_label(self, election, post_label):
+        return re.sub(r'^Member of Parliament for ', '', post_label)
+
+    def party_to_possible_post_groups(self, party_data):
+        register = party_data.get('register')
+        if register == 'Northern Ireland':
+            return ('Northern Ireland',)
+        elif register == 'Great Britain':
+            return ('England', 'Scotland', 'Wales')
+        else:
+            return ('England', 'Northern Ireland', 'Scotland', 'Wales')

elections/ar_elections_2015/lib.py
@@ -1,6 +1,44 @@
-party_sets = (
-    {'slug': 'nacionale', 'name': 'Nacionale'},
+from candidates.static_data import (
+    BaseMapItData, BasePartyData, BaseAreaPostData
 )
 
-def post_id_to_party_set(post_id):
-    return 'nacionale'
+
+class MapItData(BaseMapItData):
+    pass
+
+
+class PartyData(BasePartyData):
+
+    def __init__(self):
+        super(PartyData, self).__init__()
+        self.ALL_PARTY_SETS = (
+            {'slug': 'nacionale', 'name': 'Nacionale'},
+        )
+
+    def party_data_to_party_sets(self, party_data):
+        return ['nacionale']
+
+
+class AreaPostData(BaseAreaPostData):
+
+    def __init__(self, *args, **kwargs):
+        super(AreaPostData, self).__init__(*args, **kwargs)
+        self.ALL_POSSIBLE_POST_GROUPS = []
+
+    def area_to_post_group(self, area_data):
+        return None
+
+    def get_post_id(self, mapit_type, area_id):
+        return str(area_id)
+
+    def post_id_to_party_set(self, post_id):
+        return 'nacionale'
+
+    def post_id_to_post_group(self, election, post_id):
+        return None
+
+    def shorten_post_label(self, election, post_label):
+        return post_label
+
+    def party_to_possible_post_groups(self, party_data):
+        return ()

cached_counts/management/commands/cached_counts_create.py
@@ -3,7 +3,7 @@ from optparse import make_option
 
 from candidates.models import membership_covers_date
 from candidates.popit import PopItApiMixin, get_all_posts
-from candidates.static_data import PartyData
+from candidates.election_specific import PARTY_DATA
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -42,7 +42,7 @@ class Command(PopItApiMixin, BaseCommand):
             all_posts = list(
                 get_all_posts(post_role, embed='membership.person')
             )
-            all_parties = PartyData.party_id_to_name
+            all_parties = PARTY_DATA.party_id_to_name
             counts = {
                 'candidates': 0,
                 'parties': {
@@ -182,7 +182,7 @@ class Command(PopItApiMixin, BaseCommand):
                     values_list('object_id', flat=True)
             )
             current_parties = set(
-                PartyData.party_id_to_name.keys()
+                PARTY_DATA.party_id_to_name.keys()
             )
             parties_to_remove = parties_in_database - current_parties
             CachedCount.objects.filter(

candidates/election_specific.py
@@ -6,11 +6,9 @@ from django.conf import settings
 # candidates.election_specific
 
 imports_and_defaults = (
-    ('ALL_POSSIBLE_PARTY_POST_GROUPS', []),
-    ('party_to_possible_post_groups', lambda party_data: []),
-    ('area_to_post_group', lambda area_data: None),
-    ('party_sets', ()),
-    ('post_id_to_party_set', lambda post_id: None),
+    ('MapItData', None),
+    ('PartyData', None),
+    ('AreaPostData', None),
 )
 
 # Note that one could do this without the dynamic import and use of
@@ -36,3 +34,7 @@ for name_to_import, default_value in imports_and_defaults:
             globals()[name_to_import] = default_value
     else:
         globals()[name_to_import] = default_value
+
+MAPIT_DATA = MapItData()
+PARTY_DATA = PartyData()
+AREA_POST_DATA = AreaPostData(MAPIT_DATA, PARTY_DATA)

candidates/forms.py
@@ -2,9 +2,8 @@
 
 import re
 
-from .election_specific import party_sets
+from .election_specific import MAPIT_DATA, PARTY_DATA
 from .models.address import check_address
-from .static_data import MapItData, PartyData
 
 from django import forms
 from django.conf import settings
@@ -150,7 +149,7 @@ class BasePersonForm(forms.Form):
             # selected constituency.
             constituency = cleaned_data['constituency_' + election]
             try:
-                mapit_area = MapItData.areas_by_id[('WMC', 22)][constituency]
+                mapit_area = MAPIT_DATA.areas_by_id[('WMC', 22)][constituency]
             except KeyError:
                 message = _("If you mark the candidate as standing in the "
                             "{election}, you must select a constituency")
@@ -162,7 +161,7 @@ class BasePersonForm(forms.Form):
             else:
                 party_field = 'party_gb_' + election
             party_id = cleaned_data[party_field]
-            if party_id not in PartyData.party_id_to_name:
+            if party_id not in PARTY_DATA.party_id_to_name:
                 message = _("You must specify a party for the 2015 election")
                 raise forms.ValidationError(message)
         return cleaned_data
@@ -193,14 +192,14 @@ class NewPersonForm(BasePersonForm):
         # choice field for each such "party set" and make sure only
         # the appropriate one is shown, depending on the election and
         # selected constituency, using Javascript.
-        for party_set in party_sets:
+        for party_set in PARTY_DATA.ALL_PARTY_SETS:
             self.fields['party_' + party_set['slug'] + '_' + election] = \
                 forms.ChoiceField(
                     label=_("Party in {election} ({party_set_name})").format(
                         election=election_data['name'],
                         party_set_name=party_set['name'],
                     ),
-                    choices=PartyData.party_choices[party_set['name']],
+                    choices=PARTY_DATA.party_choices[party_set['slug']],
                     required=False,
                 )
 
@@ -254,20 +253,20 @@ class UpdatePersonForm(BasePersonForm):
                         [
                             (mapit_id, constituency['name'])
                             for mapit_id, constituency
-                            in MapItData.areas_by_id[('WMC', 22)].items()
+                            in MAPIT_DATA.areas_by_id[('WMC', 22)].items()
                         ],
                         key=lambda t: t[1]
                     ),
                     widget=forms.Select(attrs={'class': 'post-select'}),
                 )
-            for party_set in party_sets:
+            for party_set in PARTY_DATA.ALL_PARTY_SETS:
                 self.fields['party_' + party_set['slug'] + '_' + election] = \
                     forms.ChoiceField(
                         label=_("Party in {election} ({party_set_name})").format(
                             election=election_data['name'],
                             party_set_name=party_set['name'],
                         ),
-                        choices=PartyData.party_choices[party_set['name']],
+                        choices=PARTY_DATA.party_choices[party_set['slug']],
                         required=False,
                         widget=forms.Select(
                             attrs={
@@ -328,7 +327,7 @@ class ToggleLockForm(forms.Form):
 
     def clean_post_id(self):
         post_id = self.cleaned_data['post_id']
-        if post_id not in MapItData.areas_by_id[('WMC', 22)]:
+        if post_id not in MAPIT_DATA.areas_by_id[('WMC', 22)]:
             message = _('{0} was not a known post ID')
             raise ValidationError(message.format(post_id))
         return post_id

candidates/management/commands/candidates_import_ppcs.py
@@ -22,7 +22,7 @@ from django.conf import settings
 import requests
 
 from candidates.models import PopItPerson
-from candidates.static_data import MapItData
+from candidates.election_specific import MAPIT_DATA
 from candidates.views.version_data import get_change_metadata
 from candidates.popit import PopItApiMixin, get_search_url
 
@@ -73,7 +73,7 @@ def cons_key(s):
 
 constituency_lookup = {
     cons_key(k): v for k, v in
-    MapItData.areas_by_name[('WMC', 22)].items()
+    MAPIT_DATA.areas_by_name[('WMC', 22)].items()
 }
 
 class UnknownConstituencyException(Exception):

candidates/management/commands/candidates_import_statements_of_persons_nominated.py
@@ -10,7 +10,7 @@ import requests
 from django.core.management.base import BaseCommand
 from django.core.files.storage import FileSystemStorage 
 
-from candidates.static_data import MapItData
+from candidates.election_specific import MAPIT_DATA
 from official_documents.models import OfficialDocument
 
 CSV_URL = 'https://docs.google.com/a/mysociety.org/spreadsheets/d/1jvWaQSENnASZfGne1IWRbDATMH2NT2xutyPEbZ5Is-8/export?format=csv&id=1jvWaQSENnASZfGne1IWRbDATMH2NT2xutyPEbZ5Is-8&gid=0'
@@ -50,7 +50,7 @@ class Command(BaseCommand):
             name = row['Constituency'].decode('utf-8')
             if not name:
                 continue
-            cons_data = MapItData.areas_by_name[('WMC', 22)][name]
+            cons_data = MAPIT_DATA.areas_by_name[('WMC', 22)][name]
             document_url = row['URL']
             if not document_url:
                 print u"No URL for {0}".format(name)

commit 8ae7ecf3e8e39e4c84cfc942794720b6144827a3

Fix a bug in post_id_to_party_set introduced in d948b8f

candidates/static_data.py
@@ -61,7 +61,6 @@ class BaseMapItData(object):
         self.areas_by_id = {}
         self.areas_by_name = {}
         self.areas_list_sorted_by_name = {}
-        self.areas_by_post_id = {}
 
         for t, election_data in settings.MAPIT_TYPES_GENERATIONS_ELECTIONS.items():
             mapit_type, mapit_generation = t

elections/uk_general_election_2015/lib.py
@@ -46,7 +46,7 @@ class AreaPostData(BaseAreaPostData):
         return str(area_id)
 
     def post_id_to_party_set(self, post_id):
-        area = self.mapit_data.areas_by_post_id[post_id]
+        area = self.areas_by_post_id[post_id]
         if area['country_name'] == 'Northern Ireland':
             return 'ni'
         else: