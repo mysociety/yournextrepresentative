7377e093e7d0aaa2c2d01364c817d95d0675139b
lizconlan <liz@mysociety.org>
2015-06-19 17:12:22 +0100

    Add more tests for the areas view

--- candidates/tests/test_areas_view.py
+++ candidates/tests/test_areas_view.py
@@ -1,4 +1,5 @@
 from mock import patch
+import re
 
 from django_webtest import WebTest
 
@@ -6,14 +7,303 @@ from .auth import TestUserMixin
 from .fake_popit import (FakePersonCollection, FakeOrganizationCollection,
                          FakePostCollection)
 
-@patch('candidates.popit.PopIt')
-class TestConstituencyDetailView(TestUserMixin, WebTest):
 
-    def test_any_constituency_page_without_login(self, mock_popit):
+@patch('candidates.popit.PopIt')
+class TestAreasView(TestUserMixin, WebTest):
+
+    def test_any_area_page_without_login(self, mock_popit):
         mock_popit.return_value.organizations = FakeOrganizationCollection
         mock_popit.return_value.persons = FakePersonCollection
         mock_popit.return_value.posts = FakePostCollection
 
-        # Just a smoke test for the moment:
         response = self.app.get('/areas/WMC-65808/dulwich-and-west-norwood')
         self.assertEqual(response.status_code, 200)
+
+        self.assertTrue(
+            re.search(
+                r'''(?msx)
+  <h1>Candidates\s+for\s+Member\s+of\s+Parliament\s+for\s+
+  Dulwich\s+and\s+West\s+Norwood</h1>.*
+  <h3>Known\s+candidates\s+for\s*
+  <a\s+href="/election/2015/post/65808/
+  member-of-parliament-for-dulwich-and-west-norwood">
+  Member\s+of\s+Parliament\s+for\s+Dulwich\s+and\s+West\s+Norwood</a>\s*
+  </h3>''',
+                unicode(response)
+            )
+        )
+
+        # make sure there's at least one candidate
+        self.assertTrue(
+            response.html.find(
+                'li', {'class': 'candidates-list__person'}
+            )
+        )
+
+
+    def test_unlocked_area_without_login(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get('/areas/WMC-65808/dulwich-and-west-norwood')
+
+        # no editing functions should be visible
+        self.assertNotIn('Add a new candidate', response)
+
+        # should be invited to sign in to make edits
+        self.assertIn('Sign in to edit', response)
+
+        # no message about the data being locked should be visible
+        self.assertFalse(
+            re.search(
+                r'''(?msx)
+    <p>This\s+list\s+of\s+candidates\s+is\s+now\s+
+    <strong>locked</strong>''',
+                unicode(response)
+            )
+        )
+
+
+    def test_locked_area_without_login(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get('/areas/WMC-65913/camberwell-and-peckham')
+
+        # no editing functions should be visible
+        self.assertNotIn('Add a new candidate', response)
+
+        # message about the data being locked should be visible
+        self.assertTrue(
+            re.search(
+                r'''(?msx)
+    <p>This\s+list\s+of\s+candidates\s+is\s+now\s+
+    <strong>locked</strong>''',
+                unicode(response)
+            )
+        )
+
+
+    def test_unlocked_area_unauthorized(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get(
+            '/areas/WMC-65808/dulwich-and-west-norwood',
+            user=self.user_refused
+        )
+
+        # should not be invited to add a new candidate
+        self.assertNotIn('Add a new candidate', response)
+
+        # should not be invited to sign in to make edits
+        self.assertNotIn('Sign in to edit', response)
+
+        # message about the data being locked should not be visible
+        self.assertFalse(
+            re.search(
+                r'''(?msx)
+    <p>This\s+list\s+of\s+candidates\s+is\s+now\s+
+    <strong>locked</strong>''',
+                unicode(response)
+            )
+        )
+
+
+    def test_locked_area_unauthorized(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get(
+            '/areas/WMC-65913/camberwell-and-peckham',
+            user=self.user_refused
+        )
+
+        # editing functions should not be visible
+        self.assertNotIn('Add a new candidate', response)
+
+        # should not be invited to sign in to make edits
+        self.assertNotIn('Sign in to edit', response)
+
+        # message about the data being locked should not be visible
+        self.assertFalse(
+            re.search(
+                r'''(?msx)
+    <p>This\s+list\s+of\s+candidates\s+is\s+now\s+
+    <strong>locked</strong>''',
+                unicode(response)
+            )
+        )
+
+
+    def test_unlocked_area_edit_authorized(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get(
+            '/areas/WMC-65808/dulwich-and-west-norwood',
+            user=self.user
+        )
+
+        # editing functions should be visible
+        self.assertIn('Add a new candidate', response)
+
+        # should not be invited to sign in to make edits
+        self.assertNotIn('Sign in to edit', response)
+
+        # message about the data being locked should not be visible
+        self.assertFalse(
+            re.search(
+                r'''(?msx)
+    <p>This\s+list\s+of\s+candidates\s+is\s+now\s+
+    <strong>locked</strong>''',
+                unicode(response)
+            )
+        )
+
+        # option to lock the data should not be visible
+        self.assertFalse(
+            response.html.find(
+                'input', {'value': 'Lock candidate list'}
+            )
+        )
+
+
+    def test_locked_area_edit_authorized(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get(
+            '/areas/WMC-65913/camberwell-and-peckham',
+            user=self.user
+        )
+
+        # editing functions should not be visible
+        self.assertNotIn('Add a new candidate', response)
+
+        # should not be invited to sign in to make edits
+        self.assertNotIn('Sign in to edit', response)
+
+        # message about the data being locked should be visible
+        # at the top of the page
+        self.assertTrue(
+            re.search(
+                r'''(?msx)
+    <p>This\s+list\s+of\s+candidates\s+is\s+now\s+
+    <strong>locked</strong>''',
+                unicode(response)
+            )
+        )
+
+        # option to unlock the data should not be visible
+        self.assertFalse(
+            response.html.find(
+                'input', {'value': 'Unlock candidate list'}
+            )
+        )
+
+
+    def test_unlocked_area_lock_authorized(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get(
+            '/areas/WMC-65808/dulwich-and-west-norwood',
+            user=self.user_who_can_lock
+        )
+
+        # editing functions should be visible
+        self.assertIn('Add a new candidate', response)
+
+        # should not be invited to login to make edits
+        self.assertNotIn('Sign in to edit', response)
+
+        # message about the data being unlocked should be visible
+        self.assertTrue(
+            re.search(
+                r'''(?msx)
+            \(This\s+list\s+of\s+candidates\s+is\s+currently\s+
+            <strong>unlocked</strong>.\)''',
+                    unicode(response)
+            )
+        )
+
+        # option to lock the data should be visible
+        self.assertTrue(
+            response.html.find(
+                'input', {'value': 'Lock candidate list'}
+            )
+        )
+
+
+    def test_locked_area_lock_authorized(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get(
+            '/areas/WMC-65913/camberwell-and-peckham',
+            user=self.user_who_can_lock
+        )
+
+        # editing functions should be visible
+        self.assertIn('Add a new candidate', response)
+
+        # should not be invited to login to make edits
+        self.assertNotIn('Sign in to edit', response)
+
+        # message about the data being locked should be visible
+        self.assertTrue(
+            re.search(
+                r'''(?msx)
+    \(This\s+list\s+of\s+candidates\s+is\s+currently\s+
+    <strong>locked</strong>.\)''',
+                unicode(response)
+            )
+        )
+
+        # option to unlock the data should be visible
+        self.assertTrue(
+            response.html.find(
+                'input', {'value': 'Unlock candidate list'}
+            )
+        )
+
+
+    def test_area_without_winner_record_result_authorized(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.persons = FakePersonCollection
+        mock_popit.return_value.posts = FakePostCollection
+
+        response = self.app.get(
+            '/areas/WMC-65913/camberwell-and-peckham',
+            user=self.user_who_can_record_results
+        )
+
+        # should not allow recording the winner from this page
+        self.assertNotIn('This candidate won!', response)
+
+
+    def test_get_malformed_url(self, mock_popit):
+        response = self.app.get(
+            '/areas/3243452345/invalid',
+            expect_errors=True
+        )
+        self.assertEqual(response.status_code, 400)
+
+
+    def test_get_non_existent(self, mock_popit):
+        mock_popit.return_value.organizations = FakeOrganizationCollection
+        mock_popit.return_value.posts = FakePostCollection
+        response = self.app.get(
+            '/areas/WMC-11111111/imaginary-constituency',
+            expect_errors=True
+        )
+        self.assertEqual(response.status_code, 400)
