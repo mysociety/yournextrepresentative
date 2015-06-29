3054a83525610e4ffd544f877b727b7309544195
Mark Longair <mhl@pobox.com>
2015-06-29 17:50:53 +0100

    Update CSV file generation to support multiple, generic elections
    Previously the candidates_create_csv command would output a single CSV
    file either to standard output or a given filename; instead it will now
    output a CSV file per election.  If you call:
    
      ./manage.py candidates_create_csv /tmp/foo
    
    ... with the UK general election ELECTION_APP, this will output three
    files:
    
      /tmp/foo-2010.csv
      /tmp/foo-2015.csv
      /tmp/foo-all.csv
    
    The foo-2010.csv and foo-2015.csv files contain the candidates for the
    2010 and 2015 elections respectively, while the foo-all.csv files
    contains data from all known elections.
    
    This commit also updates the crontab to generate these new files, with
    different ouptut names, since this commit also reorders the column
    headings to be more coherent; it's possible that someone is still
    relying on the old file location.

--- candidates/management/commands/candidates_create_csv.py
+++ candidates/management/commands/candidates_create_csv.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
 from optparse import make_option
 from os import chmod, rename
 from os.path import dirname
 import sys
 from tempfile import NamedTemporaryFile
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from candidates.csv_helpers import list_to_csv
 from candidates.models import PopItPerson
@@ -13,40 +14,56 @@ from candidates.popit import PopItApiMixin, popit_unwrap_pagination
 
 class Command(PopItApiMixin, BaseCommand):
 
+    help = "Output CSV files for all elections"
+    args = "<BASE-OUTPUT-FILENAME>"
+
     option_list = BaseCommand.option_list + (
         make_option('-o', '--output',
                     dest='output_filename',
                     help='The filename to write CSV to'),
-        make_option('-y', '--year',
-                    dest='year',
-                    help='The election year',
+        make_option('-e', '--election',
+                    dest='election',
+                    help='The election identifier',
                     default='2015'),
     )
 
-    def handle(self, **options):
+    def handle(self, *args, **options):
+        if len(args) != 1:
+            msg = "You must supply the prefix for output filenames"
+            raise CommandError(msg)
+        output_prefix = args[0]
         all_people = []
+        election_to_people = defaultdict(list)
         for person_dict in popit_unwrap_pagination(
                 self.api.persons,
                 embed="membership.organization",
                 per_page=100,
         ):
-            if person_dict.get('standing_in') \
-                and person_dict['standing_in'].get(options['year']):
+            standing_in = person_dict.get('standing_in')
+            if not standing_in:
+                continue
+            for election in standing_in.keys():
+                if not standing_in[election]:
+                    continue
                 person = PopItPerson.create_from_dict(person_dict)
-                all_people.append(person.as_dict(year=options['year']))
-        csv = list_to_csv(all_people)
-        # Write to stdout if no output filename is specified, or if it
-        # is '-'
-        if options['output_filename'] in (None, '-'):
-            with sys.stdout as f:
-                f.write(csv)
-        else:
+                person_as_csv_dict = person.as_dict(election=election)
+                all_people.append(person_as_csv_dict)
+                election_to_people[election].append(person_as_csv_dict)
+        elections = election_to_people.keys() + [None]
+        for election in elections:
+            if election is None:
+                output_filename = output_prefix + '-all.csv'
+                people_data = all_people
+            else:
+                output_filename = output_prefix + '-' + election + '.csv'
+                people_data = election_to_people[election]
+            csv = list_to_csv(people_data)
             # Otherwise write to a temporary file and atomically
             # rename into place:
             ntf = NamedTemporaryFile(
                 delete=False,
-                dir=dirname(options['output_filename'])
+                dir=dirname(output_filename)
             )
             ntf.write(csv)
             chmod(ntf.name, 0o644)
-            rename(ntf.name, options['output_filename'])
+            rename(ntf.name, output_filename)
