31f2e0a7bd4d170befc2fa2eb7fb002c9def167b
lizconlan <liz@mysociety.org>
2015-07-02 18:37:49 +0100

    Split list of csv files into Current, Historic and All

--- candidates/views/help.py
+++ candidates/views/help.py
@@ -9,12 +9,20 @@ class HelpApiView(PopItApiMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HelpApiView, self).get_context_data(**kwargs)
 
-        context['csv_list'] = []
+        context['current_csv_list'] = []
         for election, election_data in settings.ELECTIONS_CURRENT:
-            context['csv_list'].append({'slug': election, 'name': election_data['name']})
+            context['current_csv_list'].append({'slug': election, 'name': election_data['name']})
+
+        context['historic_csv_list'] = []
+        current_slugs = [election['slug'] for election in context['current_csv_list']]
+        for election, election_data in settings.ELECTIONS_BY_DATE:
+            if election not in current_slugs:
+                context['historic_csv_list'].append(
+                    {'slug': election, 'name': election_data['name']})
 
         context['popit_url'] = get_base_url()
         return context
+
 
 class HelpAboutView(TemplateView):
     template_name = 'candidates/about.html'
