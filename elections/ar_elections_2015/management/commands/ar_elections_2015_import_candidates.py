3af752d92b78347412eeb35f3c656901dc455613
Mark Longair <mhl@pobox.com>
2015-07-20 16:00:58 +0100

    AR: Make the candidate import script idempotent
    This change means the script now records the person ID from the
    JSON which is imported and uses it to check if the person already
    exists on running the script; if so, that existing person is updated
    rather than creating a new one in PopIt.
    
    Fixes #451, fixes #450

--- elections/ar_elections_2015/management/commands/ar_elections_2015_import_candidates.py
+++ elections/ar_elections_2015/management/commands/ar_elections_2015_import_candidates.py
@@ -17,7 +17,7 @@ from django.core.management.base import BaseCommand, CommandError
 from candidates.cache import get_post_cached
 from candidates.election_specific import MAPIT_DATA, PARTY_DATA, AREA_POST_DATA
 from candidates.models import PopItPerson
-from candidates.popit import create_popit_api_object
+from candidates.popit import create_popit_api_object, get_search_url
 from candidates.utils import strip_accents
 from candidates.views.version_data import get_change_metadata
 from moderation_queue.models import QueuedImage
@@ -87,6 +87,29 @@ def enqueue_image(person, user, image_url):
         user=user
     )
 
+def get_existing_popit_person(vi_person_id):
+    # See if this person already exists by searching for the
+    # ID they were imported with:
+    query_format = \
+        'identifiers.identifier:"{id}" AND ' + \
+        'identifiers.scheme:"{scheme}"'
+    search_url = get_search_url(
+        'persons',
+        query_format.format(
+            id=vi_person_id, scheme='import-id'
+        ),
+        embed='membership.organization'
+    )
+    results = requests.get(search_url).json()
+    total = results['total']
+    if total > 1:
+        message = "Multiple matches for CI ID {0}"
+        raise Exception(message.format(vi_person_id))
+    if total == 0:
+        return None
+    # Otherwise there was exactly one result:
+    return PopItPerson.create_from_dict(results['result'][0])
+
 
 class Command(BaseCommand):
 
@@ -143,12 +166,21 @@ class Command(BaseCommand):
             name = person_data['name']
             gender = person_data['gender']
             image_url = person_data['image']
-            # For the moment, assume the person doesn't exist:
-            person = PopItPerson()
+
+            person = get_existing_popit_person(vi_person_id)
+            if person:
+                print "Found an existing person:", person.get_absolute_url()
+            else:
+                print "No existing person, creating a new one:"
+                person = PopItPerson()
+
+            # Now update fields from the imported data:
             person.name = name
             person.gender = gender
             if birth_date:
                 person.birth_date = str(birth_date)
+            else:
+                person.birth_date = None
             standing_in_election = {
                 'post_id': post_data['id'],
                 'name': AREA_POST_DATA.shorten_post_label(
@@ -167,6 +199,7 @@ class Command(BaseCommand):
                     'name': PARTY_DATA.party_id_to_name[UNKNOWN_PARTY_ID],
                 }
             }
+            person.set_identifier('import-id', vi_person_id)
             change_metadata = get_change_metadata(
                 None,
                 'Imported candidate from JSON',
