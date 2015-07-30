c26272b5d22e74978d4bcacadc5e51c69630534d
@martinszy <martinsz@gmail.com>
2015-07-30 16:28:31 -0300

    Adding posts for mercosur parliament

--- elections/ar_elections_2015/settings.py
+++ elections/ar_elections_2015/settings.py
@@ -81,6 +81,42 @@ ELECTIONS = {
         'post_id_format': 'presidente',
         'show_official_documents': False,
     },
+
+    # Party primaries for national senators
+    'parlamentarios-mercosur-regional-paso-2015': {
+        'for_post_role': 'Parlamentario Mercosur',
+        'candidate_membership_role': 'Primary Candidate',
+        'winner_membership_role': 'Candidate',
+        'election_date': date(2015, 8, 9),
+        'candidacy_start_date': date(2015, 6, 22),
+        'name': 'Parlamentario Mercosur PASO 2015',
+        'current': True,
+        'party_membership_start_date': date(2015, 6, 22),
+        'party_membership_end_date': date(9999, 12, 31),
+        'mapit_types': ['PRV'],
+        'mapit_generation': 1,
+        'organization_id': 'parlmercosur',
+        'organization_name': 'Parlamento del Mercosur',
+        'post_id_format': 'pmer-{area_id}',
+    },
+
+    'parlamentarios-mercosur-unico-paso-2015': {
+        'for_post_role': 'Parlamentario Mercosur',
+        'candidate_membership_role': 'Primary Candidate',
+        'winner_membership_role': 'Candidate',
+        'election_date': date(2015, 8, 9),
+        'candidacy_start_date': date(2015, 6, 22),
+        'name': 'Parlamentario Mercosur PASO 2015',
+        'current': True,
+        'party_membership_start_date': date(2015, 6, 22),
+        'party_membership_end_date': date(9999, 12, 31),
+        'mapit_types': ['NAT'], # The national level...
+        'mapit_generation': 1,
+        'organization_id': 'parlmercosur',
+        'organization_name': 'Parlamento del Mercosur',
+        # There's only one such post:
+        'post_id_format': 'pmeu',
+    },    
 }
 
 MAPIT_BASE_URL = 'http://argentina.mapit.staging.mysociety.org/'
