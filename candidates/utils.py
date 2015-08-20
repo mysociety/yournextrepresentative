f43b6123db99181f392f172384f769abf2854c28
Mark Longair <mhl@pobox.com>
2015-08-20 15:54:18 +0100

    strip_accents shouldn't fail if passed a str

--- candidates/utils.py
+++ candidates/utils.py
@@ -3,6 +3,6 @@ import unicodedata
 # From http://stackoverflow.com/a/517974/223092
 def strip_accents(s):
     return u"".join(
-        c for c in unicodedata.normalize('NFKD', s)
+        c for c in unicodedata.normalize('NFKD', unicode(s))
         if not unicodedata.combining(c)
     )
