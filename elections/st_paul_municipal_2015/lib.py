37dafb818fe5f231df70b01c4cb53c5f8e137737
Eric van Zanten <eric.vanzanten@gmail.com>
2015-09-28 10:15:19 -0500

    Remove unneeded code, modules and templates. Restore needed SCSS.

--- elections/st_paul_municipal_2015/lib.py
+++ elections/st_paul_municipal_2015/lib.py
@@ -33,32 +33,3 @@ class AreaPostData(BaseAreaPostData):
         return 'st-paul'
 
 
-EXTRA_CSV_ROW_FIELDS = [
-    'gss_code',
-    'parlparse_id',
-    'theyworkforyou_url',
-    'party_ec_id',
-]
-
-def get_extra_csv_values(person, election, mapit_data):
-    theyworkforyou_url = None
-    parlparse_id = person.get_identifier('uk.org.publicwhip')
-    if parlparse_id:
-        m = re.search(r'^uk.org.publicwhip/person/(\d+)$', parlparse_id)
-        if not m:
-            message = "Malformed parlparse ID found {0}"
-            raise Exception, message.format(parlparse_id)
-        parlparse_person_id = m.group(1)
-        theyworkforyou_url = 'http://www.theyworkforyou.com/mp/{0}'.format(
-            parlparse_person_id
-        )
-    party_ec_id = person.parties[election].get('electoral_commission_id', '')
-
-    return {
-        'gss_code': mapit_data.areas_by_id[('WMC', 22)][
-            person.standing_in[election]['post_id']
-        ]['codes']['gss'],
-        'parlparse_id': parlparse_id,
-        'theyworkforyou_url': theyworkforyou_url,
-        'party_ec_id': party_ec_id
-    }
--- elections/st_paul_municipal_2015/views/__init__.py
+++ elections/st_paul_municipal_2015/views/__init__.py
@@ -1,5 +1,2 @@
-from .constituencies import *
 from .frontpage import *
-from .parties import *
-from .redirects import *
 from .areas import *
