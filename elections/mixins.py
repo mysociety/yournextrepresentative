e41d46fe0539aa102a8af92236add5ba876db7a0
Struan Donald <struan@exo.org.uk>
2015-09-28 17:40:11 +0100

    Use the Election model in the elections Mixin

--- elections/mixins.py
+++ elections/mixins.py
@@ -1,16 +1,14 @@
-from django.conf import settings
-from django.http import Http404
 from django.utils.translation import ugettext as _
+from django.shortcuts import get_object_or_404
 
+from models import Election
 
 class ElectionMixin(object):
     '''A mixin to add election data from the URL to the context'''
 
     def dispatch(self, request, *args, **kwargs):
         self.election = election = self.kwargs['election']
-        if election not in settings.ELECTIONS:
-            raise Http404(_("Unknown election: '{election}'").format(election=election))
-        self.election_data = settings.ELECTIONS[election]
+        self.election_data = get_object_or_404(Election, slug=election)
         return super(ElectionMixin, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
