39729eb3e99f5925aa5b00401238fc588e73a244
Mark Longair <mhl@pobox.com>
2015-10-13 18:08:52 +0100

    Show errors from PopIt before re-raising when creating organizations

--- candidates/management/commands/candidates_create_popit_organizations.py
+++ candidates/management/commands/candidates_create_popit_organizations.py
@@ -5,28 +5,36 @@ from candidates.election_specific import PARTY_DATA
 from candidates.models.popit import create_or_update
 from candidates.popit import PopItApiMixin
 
+from slumber.exceptions import HttpClientError, HttpServerError
+
 class Command(PopItApiMixin, BaseCommand):
     help = "Create required organizations (parties / chambers) in PopIt"
 
     def handle(self, **options):
 
-        # First create all the political parties:
+        try:
 
-        for party_data in PARTY_DATA.all_party_data:
-            create_or_update(
-                self.api.organizations,
-                party_data,
-            )
+            # First create all the political parties:
 
-        # Now we create the organizations that all the posts are
-        # associated with:
+            for party_data in PARTY_DATA.all_party_data:
+                create_or_update(
+                    self.api.organizations,
+                    party_data,
+                )
 
-        for election, election_data in settings.ELECTIONS.items():
+            # Now we create the organizations that all the posts are
+            # associated with:
 
-            create_or_update(
-                self.api.organizations,
-                {
-                    'id': election_data['organization_id'],
-                    'name': election_data['organization_name'],
-                }
-            )
+            for election, election_data in settings.ELECTIONS.items():
+
+                create_or_update(
+                    self.api.organizations,
+                    {
+                        'id': election_data['organization_id'],
+                        'name': election_data['organization_name'],
+                    }
+                )
+
+        except (HttpServerError, HttpClientError) as http_error:
+            print "The body of the error was:", http_error.content
+            raise
