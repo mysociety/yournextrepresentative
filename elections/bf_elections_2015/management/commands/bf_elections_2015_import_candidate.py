5c1ce002941d546bde49721f98b51aaecaaf386f
Mark Longair <mhl@pobox.com>
2015-10-15 13:23:05 +0100

    Simpler handling of Windows 1252 decoding

--- elections/bf_elections_2015/management/commands/bf_elections_2015_import_candidate.py
+++ elections/bf_elections_2015/management/commands/bf_elections_2015_import_candidate.py
@@ -98,17 +98,10 @@ def get_party_data(party_name):
     # Otherwise there was exactly one result:
     return results['result'][0]
 
-def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
-    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
-                            dialect=dialect, **kwargs)
-    for row in csv_reader:
-        # decode UTF-8 back to Unicode, cell by cell:
-        yield [unicode(cell, 'utf-8') for cell in row]
+def windows_1252_csv_reader(csvfile):
+    for encoded_row in csv.reader(csvfile):
+        yield [c.decode('windows-1252') for c in encoded_row]
 
-def utf_8_encoder(unicode_csv_data):
-    for line in unicode_csv_data:
-        yield line.encode('utf-8')
 
 class Command(BaseCommand):
 
@@ -158,11 +151,10 @@ class Command(BaseCommand):
 
             fields = field_map[election_id]
 
-
-            with codecs.open(csv_filename, 'r', encoding='windows-1252') as f:
+            with open(csv_filename) as f:
 
                 initial = True
-                for candidate in unicode_csv_reader(f):
+                for candidate in windows_1252_csv_reader(f):
                     # skip header line
                     if initial:
                         initial = False
