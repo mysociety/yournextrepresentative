d65ae50ea993eddde867b607308252e84b2310b2
Mark Longair <mhl@pobox.com>
2015-11-01 17:29:39 +0000

    Remove an unused import

--- candidates/migrations/0008_migrate_to_django_popolo.py
+++ candidates/migrations/0008_migrate_to_django_popolo.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import re
 
-from django.db import models, migrations
+from django.db import migrations
 
 from popolo.importers.popit import PopItImporter
 
