1574e76ac4971e27a22297648c32189d31d99947
Struan Donald <struan@exo.org.uk>
2015-11-12 11:57:23 +0000

    update uk party views to use django-popolo models

--- elections/uk_general_election_2015/views/parties.py
+++ elections/uk_general_election_2015/views/parties.py
@@ -6,11 +6,11 @@ class UKPartyDetailView(PartyDetailView):
     def get_context_data(self, **kwargs):
         context = super(UKPartyDetailView, self).get_context_data(**kwargs)
 
-        party_ec_id = get_identifier('electoral-commission', context['party'])
+        party_ec_id = context['party'].identifiers.get(scheme='electoral-commission')
         context['ec_url'] = None
         if party_ec_id:
             ec_tmpl = 'http://search.electoralcommission.org.uk/English/Registrations/{0}'
             context['ec_url'] = ec_tmpl.format(party_ec_id)
-        context['register'] = context['party'].get('register')
+        context['register'] = context['party'].extra.register
 
         return context
