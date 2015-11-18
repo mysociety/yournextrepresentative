ced455c502dc9d3852314a21c9739ee4be59e3f5
Struan Donald <struan@exo.org.uk>
2015-11-18 15:45:26 +0000

    update get_redirect_to_post function with ID change
    pass post slug and not id to url constructor

--- candidates/views/helpers.py
+++ candidates/views/helpers.py
@@ -22,7 +22,7 @@ def get_redirect_to_post(election, post):
             'constituency',
             kwargs={
                 'election': election,
-                'post_id': post.id,
+                'post_id': post.extra.slug,
                 'ignored_slug': slugify(short_post_label),
             }
         )
