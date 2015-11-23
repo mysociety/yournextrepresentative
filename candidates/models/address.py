2868c1a652fdd290eb23c912ad7218397ffa5ad9
Mark Longair <mhl@pobox.com>
2015-11-23 13:18:09 +0000

    Update address lookup to replace now-removed settings
    The commit f99179ef55f05792e51 removed the MAPIT_TYPE and
    MAPIT_CURRENT_GENERATION settings, but the generic address lookup wasn't
    updated - this caused address lookup on Burkina Faso error.

--- candidates/models/address.py
+++ candidates/models/address.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.utils.text import slugify
@@ -5,6 +7,8 @@ from django.utils.translation import ugettext as _
 
 from pygeocoder import Geocoder, GeocoderError
 import requests
+
+from elections.models import Election
 
 # We use this both for validation of address and the results of the
 # lookup, so the MapIt and geocoder lookups should be cached so we
@@ -22,22 +26,27 @@ def check_address(address_string, country=None):
         message = _(u"Failed to find a location for '{0}'")
         raise ValidationError(message.format(tidied_address_before_country))
     lat, lon = location_results[0].coordinates
-    mapit_lookup_url = '{base_url}point/4326/{lon},{lat}'.format(
-        base_url=settings.MAPIT_BASE_URL,
-        lon=lon,
-        lat=lat,
-    )
-    mapit_lookup_url += '?type=' + ','.join(settings.MAPIT_TYPES)
-    mapit_lookup_url += '&generation={0}'.format(
-        settings.MAPIT_CURRENT_GENERATION
-    )
-    mapit_result = requests.get(mapit_lookup_url)
-    mapit_json = mapit_result.json()
-    if 'error' in mapit_json:
-        message = _(u"The area lookup returned an error: '{error}'")
-        raise ValidationError(message.format(error=mapit_json['error']))
+    all_mapit_json = []
+    queries_to_try = defaultdict(set)
+    for election in Election.objects.current().prefetch_related('area_types'):
+        area_types = election.area_types.values_list('name', flat=True)
+        queries_to_try[election.area_generation].update(area_types)
+    for area_generation, area_types in queries_to_try.items():
+        mapit_lookup_url = '{base_url}point/4326/{lon},{lat}'.format(
+            base_url=settings.MAPIT_BASE_URL,
+            lon=lon,
+            lat=lat,
+        )
+        mapit_lookup_url += '?type=' + ','.join(area_types)
+        mapit_lookup_url += '&generation={0}'.format(election.area_generation)
+        mapit_result = requests.get(mapit_lookup_url)
+        mapit_json = mapit_result.json()
+        if 'error' in mapit_json:
+            message = _(u"The area lookup returned an error: '{error}'")
+            raise ValidationError(message.format(error=mapit_json['error']))
+        all_mapit_json += mapit_json.items()
     sorted_mapit_results = sorted(
-        mapit_json.items(),
+        all_mapit_json,
         key=lambda t: (t[1]['type'], int(t[0]))
     )
     if not sorted_mapit_results:
