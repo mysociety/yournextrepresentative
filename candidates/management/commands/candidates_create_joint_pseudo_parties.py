0368a932de2f20d8b18975aeb0dfd08d8092392d
Mark Longair <mhl@pobox.com>
2015-11-29 09:53:56 +0000

    Update the candidates_create_joint_pseudo_parties command for django-popolo

--- candidates/management/commands/candidates_create_joint_pseudo_parties.py
+++ candidates/management/commands/candidates_create_joint_pseudo_parties.py
@@ -2,12 +2,12 @@ from collections import defaultdict
 from datetime import date
 import re
 
-from slumber.exceptions import HttpServerError
-
 from django.core.management.base import BaseCommand
-from django.utils.text import slugify
+from django.db import transaction
 
-from candidates.popit import create_popit_api_object, popit_unwrap_pagination
+from popolo.models import Organization
+
+from candidates.models import OrganizationExtra
 
 def fix_joint_party_name(joint_party_name):
     return {
@@ -21,32 +21,27 @@ def extract_number_from_id(party_id):
         return int(m.group(0), 10)
     return -1
 
-def create_or_update_party(api, joint_party_name, sub_parties):
+def create_or_update_party(joint_party_name, sub_parties):
     party_ids = sorted(
-        extract_number_from_id(p['id'])
+        extract_number_from_id(p.extra.slug)
         for p in sub_parties
     )
-    registers = set(p['register'] for p in sub_parties)
+    registers = set(p.extra.register for p in sub_parties)
     if len(registers) > 1:
         raise Exception("Multiple registers found " + repr(registers))
     joint_party_id = 'joint-party:' + '-'.join(str(i) for i in party_ids)
-    party_data = {
-        'id': joint_party_id,
-        'name': joint_party_name,
-        'slug': slugify(joint_party_name),
-        'classification': 'Party',
-        'descriptions': [],
-        'dissolution_date': '9999-12-31',
-        'register': next(iter(registers)),
-    }
-    try:
-        api.organizations.post(party_data)
-    except HttpServerError as e:
-        if 'E11000' in e.content:
-            # Duplicate Party Found
-            api.organizations(joint_party_id).put(party_data)
-        else:
-            raise
+    if not OrganizationExtra.objects.filter(slug=joint_party_id).exists():
+        joint_party = Organization.objects.create(
+            name=joint_party_name,
+            classification='Party',
+            dissolution_date='9999-12-31',
+        )
+        OrganizationExtra.objects.create(
+            base=joint_party,
+            slug=joint_party_id,
+            register=next(iter(registers)),
+        )
+
 
 joint_description_re = re.compile(
     r'^(?P<joint_name>.*?) \([jJ]oint [dD]escriptions? with (?P<sub_party>.*)\)'
@@ -58,30 +53,37 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         joint_party_to_sub_parties = defaultdict(list)
-        api = create_popit_api_object()
-        for party in popit_unwrap_pagination(
-                api.organizations,
-                embed='',
-                per_page=200
-        ):
-            if party['classification'] != 'Party':
+        for party_extra in OrganizationExtra.objects.filter(
+                base__classification='Party'
+        ).select_related('base'):
+            party = party_extra.base
+            today_str = str(date.today())
+            if party.dissolution_date and (party.dissolution_date < today_str):
                 continue
-            if 'dissolution_date' in party:
-                dissolution_date = party['dissolution_date']
-                if dissolution_date < str(date.today()):
-                    continue
-            for d in party.get('descriptions', []):
-                m = joint_description_re.search(d['description'])
+            descriptions = party.other_names.filter(
+                note='registered-description'
+            )
+            for description_with_translation in descriptions:
+                d = re.split(r'\s*\|\s*', description_with_translation.name)[0]
+                m = joint_description_re.search(d)
                 if not m:
                     continue
                 joint_name = fix_joint_party_name(m.group('joint_name'))
                 joint_party_to_sub_parties[joint_name].append(party)
-        for joint_name, sub_parties in joint_party_to_sub_parties.items():
-            if len(sub_parties) < 2:
-                message = 'Ignoring "{joint_name}" (only made up of one party: "{sub_party}")'
+        with transaction.atomic():
+            for joint_name, sub_parties in joint_party_to_sub_parties.items():
+                if len(sub_parties) < 2:
+                    message = 'Ignoring "{joint_name}"' \
+                        ' (only made up of one party: "{sub_party}")'
+                    print message.format(
+                        joint_name=joint_name,
+                        sub_party=sub_parties[0].name
+                    )
+                    continue
+                message = 'Creating "{joint_name}", made up of: {sub_parties}'
                 print message.format(
                     joint_name=joint_name,
-                    sub_party=sub_parties[0]['name']
+                    sub_parties=sub_parties,
                 )
-                continue
-            create_or_update_party(api, joint_name, sub_parties)
+
+                create_or_update_party(joint_name, sub_parties)
