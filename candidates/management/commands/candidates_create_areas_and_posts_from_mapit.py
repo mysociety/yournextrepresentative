58da7f82c03d3601a6dee17986a33eb35aae1bc9
Struan Donald <struan@exo.org.uk>
2015-12-01 17:57:06 +0000

    management command to create areas and posts WIP
    Given the MapIt URL of a region, an area type and a post id format this
    will add an area and a post, plus associated other entities, for each
    area of the given type covered by the region.
    
    It will also link the posts to all the elections present.
    
    Currently assumes that all the elections are the same but for different
    years and that all posts are part of all elections. This clearly is not
    always going to be the case so needs some work.

---
+++ candidates/management/commands/candidates_create_areas_and_posts_from_mapit.py
@@ -0,0 +1,92 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+
+from optparse import make_option
+from urlparse import urljoin
+import requests
+
+from popolo.models import Post, Area
+from candidates.models import PostExtra, AreaExtra
+from elections.models import Election, AreaType
+
+class Command(BaseCommand):
+    args = "<MAPIT-URL> <AREA_TYPE> <POST_ID_FORMAT>"
+    help = "Create areas and posts based on a mapit area type"
+
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--post-label',
+            help='Override the format string used to construct the post label [default: "%default"]',
+            default=_('{post_role} for {area_name}'),
+        ),
+    )
+
+    def handle(self, *args, **options):
+        post_label_format = _('{post_role} for {area_name}')
+        if options['post_label']:
+            post_label_format = options['post_label']
+
+        if len(args) != 3:
+            raise CommandError("You must provide all three arguments")
+
+        mapit_url, area_type, post_id_format = args
+
+        all_areas_url = mapit_url + '/covers' + '?type=' + area_type
+        mapit_result = requests.get(all_areas_url)
+        mapit_json = mapit_result.json()
+
+        elections = Election.objects.all()
+
+        election = elections.first()
+
+        if election is None:
+            raise CommandError("There must be at least one election")
+
+        for_post_role = election.for_post_role
+        org = election.organization
+
+        for item in mapit_json.items():
+            area_json = item[1]
+
+            area_url = urljoin(mapit_url, '/area/' + str(area_json['id']))
+
+            area, area_created = Area.objects.get_or_create(
+                name=area_json['name'],
+                identifier=area_url,
+                classification=area_json['type_name']
+            )
+
+            area_type, created = AreaType.objects.get_or_create(
+                name=area_json['type'],
+                source='MapIt'
+            )
+
+            if area_created:
+                area_extra, area_extra_created = AreaExtra.objects.get_or_create(
+                    base=area
+                )
+
+            if area_created and area_extra_created:
+                area_extra.type = area_type
+                area_extra.save()
+
+            post_id = post_id_format.format(area_id=area_json['id'])
+            post_name = post_label_format.format(
+                area_name=area_json['name'],
+                post_role=for_post_role
+            )
+
+            post, created = Post.objects.get_or_create(
+                label=post_name,
+                area=area,
+                organization=org
+            )
+
+            post_extra, created = PostExtra.objects.get_or_create(
+                base=post,
+                slug=post_id
+            )
+
+            for election in elections:
+                post_extra.elections.add(election)
+
