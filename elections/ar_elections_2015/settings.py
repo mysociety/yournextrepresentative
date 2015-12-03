7536e467b126465bbb42a9167be546dfb98c654a
Mark Longair <mhl@pobox.com>
2015-12-03 17:29:00 +0000

    fixup! update apps to use the Election model

--- elections/ar_elections_2015/settings.py
+++ elections/ar_elections_2015/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-ELECTION_RE = '(?P<election>diputados-argentina-paso-2015|gobernadores-argentina-paso-2015|senadores-argentina-paso-2015|presidentes-argentina-paso-2015)'
+ELECTION_RE = '(?P<election>diputados-argentina-paso-2015|gobernadores-argentina-paso-2015|senadores-argentina-paso-2015|presidentes-argentina-paso-2015|parlamentarios-mercosur-regional-paso-2015|parlamentarios-mercosur-unico-paso-2015)'
 MAPIT_BASE_URL = 'http://argentina.mapit.staging.mysociety.org/'
 
 SITE_OWNER = 'YoQuieroSaber'
