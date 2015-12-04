006da1cc000e458eb346054b4f4003f82901a428
Mark Longair <mhl@pobox.com>
2015-12-04 14:01:55 +0000

    Record a new version when using candidates_add_identifier

--- candidates/management/commands/candidates_add_identifier.py
+++ candidates/management/commands/candidates_add_identifier.py
@@ -1,6 +1,8 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from popolo.models import Person
+
+from candidates.views.version_data import get_change_metadata
 
 class Command(BaseCommand):
 
@@ -19,6 +21,9 @@ class Command(BaseCommand):
             'IDENTIFIER',
             help='The identifier to add'
         )
+        parser.add_argument(
+            '--source', help='The source of information for this new identifier'
+        )
 
     def handle(self, *args, **options):
         person = Person.objects.get(pk=options['PERSON-ID'])
@@ -26,5 +31,14 @@ class Command(BaseCommand):
             scheme=options['SCHEME'],
             identifier=options['IDENTIFIER'],
         )
+
+        if options['source']:
+            source = options['source']
+        else:
+            source = "Added from the command-line with no source supplied"
+
+        change_metadata = get_change_metadata(None, source)
+        person.extra.record_version(change_metadata)
+        person.extra.save()
 
         print "Successfully updated {0}".format(person.name)
