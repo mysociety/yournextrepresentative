2742138bd7237ecb04aa5b8c21292d3af9b994a3
Mark Longair <mhl@pobox.com>
2015-12-04 11:44:03 +0000

    Fix the admin view of LoggedAction objects and lists

--- candidates/admin.py
+++ candidates/admin.py
@@ -12,21 +12,21 @@ class LoggedActionAdminForm(ModelForm):
 class LoggedActionAdmin(admin.ModelAdmin):
     form = LoggedActionAdminForm
     search_fields = ('user__username', 'popit_person_new_version',
-                     'popit_person_id', 'ip_address', 'source')
+                     'person', 'ip_address', 'source')
     list_filter = ('action_type',)
     list_display = ['user', 'ip_address', 'action_type',
-                    'popit_person_new_version', 'popit_person_link',
+                    'popit_person_new_version', 'person_link',
                     'created', 'updated', 'source']
     ordering = ('-created',)
 
-    def popit_person_link(self, o):
-        if not o.popit_person_id:
+    def person_link(self, o):
+        if not o.person:
             return ''
-        url = reverse('person-view', kwargs={'person_id': o.popit_person_id})
+        url = reverse('person-view', kwargs={'person_id': o.person.id})
         return '<a href="{0}">{1}</a>'.format(
             url,
-            o.popit_person_id,
+            o.person.name,
         )
-    popit_person_link.allow_tags = True
+    person_link.allow_tags = True
 
 admin.site.register(LoggedAction, LoggedActionAdmin)
