f814a2147990e07a68b828d6bcfc30aa6edec555
Struan Donald <struan@exo.org.uk>
2015-12-15 17:41:44 +0000

    basic API tests
    cover basic functions of the API before changing it to use slugs instead
    of IDs

---
+++ candidates/tests/test_api.py
@@ -0,0 +1,351 @@
+from django_webtest import WebTest
+
+from .factories import (
+    AreaExtraFactory, AreaTypeFactory, ElectionFactory,
+    PostExtraFactory, ParliamentaryChamberFactory,
+    PersonExtraFactory, CandidacyExtraFactory, PartyExtraFactory,
+    PartyFactory, MembershipFactory, PartySetFactory
+)
+
+from candidates.models import LoggedAction
+
+
+class TestAPI(WebTest):
+
+    def setUp(self):
+        wmc_area_type = AreaTypeFactory.create()
+        gb_parties = PartySetFactory.create(slug='gb', name='Great Britain')
+        commons = ParliamentaryChamberFactory.create()
+
+        self.election = ElectionFactory.create(
+            slug='2015',
+            name='2015 General Election',
+            area_types=(wmc_area_type,),
+            organization=commons
+        )
+        old_election = ElectionFactory.create(
+            slug='2010',
+            name='2010 General Election',
+            area_types=(wmc_area_type,),
+            organization=commons
+        )
+
+        PartyFactory.reset_sequence()
+        PartyExtraFactory.reset_sequence()
+        party_extra = PartyExtraFactory.create()
+        gb_parties.parties.add(party_extra.base)
+
+        dulwich_area_extra = AreaExtraFactory.create(
+            base__identifier='65808',
+            base__name='Dulwich and West Norwood',
+            type=wmc_area_type,
+        )
+
+        post_extra = PostExtraFactory.create(
+            elections=(self.election,),
+            base__organization=commons,
+            base__area=dulwich_area_extra.base,
+            slug='65808',
+            base__label='Member of Parliament for Dulwich and West Norwood',
+            party_set=gb_parties,
+        )
+        winner_post_extra = PostExtraFactory.create(
+            elections=(self.election,),
+            base__organization=commons,
+            slug='14419',
+            base__label='Member of Parliament for Edinburgh East',
+            party_set=gb_parties,
+        )
+
+        person_extra = PersonExtraFactory.create(
+            base__id='2009',
+            base__name='Tessa Jowell'
+        )
+        dulwich_not_stand = PersonExtraFactory.create(
+            base__id='4322',
+            base__name='Helen Hayes'
+        )
+        edinburgh_candidate = PersonExtraFactory.create(
+            base__id='818',
+            base__name='Sheila Gilmore'
+        )
+        edinburgh_winner = PersonExtraFactory.create(
+            base__id='5795',
+            base__name='Tommy Sheppard'
+        )
+        edinburgh_may_stand = PersonExtraFactory.create(
+            base__id='5163',
+            base__name='Peter McColl'
+        )
+
+        CandidacyExtraFactory.create(
+            election=self.election,
+            base__person=person_extra.base,
+            base__post=post_extra.base,
+            base__on_behalf_of=party_extra.base
+            )
+        MembershipFactory.create(
+            person=person_extra.base,
+            organization=party_extra.base
+        )
+
+        CandidacyExtraFactory.create(
+            election=old_election,
+            base__person=dulwich_not_stand.base,
+            base__post=post_extra.base,
+            base__on_behalf_of=party_extra.base,
+            )
+        dulwich_not_stand.not_standing.add(self.election)
+
+        CandidacyExtraFactory.create(
+            election=self.election,
+            base__person=edinburgh_winner.base,
+            base__post=winner_post_extra.base,
+            base__on_behalf_of=party_extra.base,
+            elected=True,
+            )
+
+        CandidacyExtraFactory.create(
+            election=self.election,
+            base__person=edinburgh_candidate.base,
+            base__post=winner_post_extra.base,
+            base__on_behalf_of=party_extra.base
+            )
+        MembershipFactory.create(
+            person=edinburgh_candidate.base,
+            organization=party_extra.base
+        )
+
+        MembershipFactory.create(
+            person=edinburgh_winner.base,
+            organization=party_extra.base
+        )
+        CandidacyExtraFactory.create(
+            election=old_election,
+            base__person=edinburgh_may_stand.base,
+            base__post=winner_post_extra.base,
+            base__on_behalf_of=party_extra.base
+            )
+
+    def test_api_basic_response(self):
+        response = self.app.get(
+            '/api/v0.9/'
+        )
+        self.assertEquals(response.status_code, 200)
+        json = response.json
+
+        self.assertEquals(
+            json['persons'],
+            'http://localhost:80/api/v0.9/persons/'
+        )
+        self.assertEquals(json['areas'], 'http://localhost:80/api/v0.9/areas/')
+        self.assertEquals(
+            json['organizations'],
+            'http://localhost:80/api/v0.9/organizations/'
+        )
+        self.assertEquals(
+            json['elections'],
+            'http://localhost:80/api/v0.9/elections/'
+        )
+        self.assertEquals(json['posts'], 'http://localhost:80/api/v0.9/posts/')
+
+        persons_resp = self.app.get('/api/v0.9/persons/')
+        self.assertEquals(persons_resp.status_code, 200)
+
+        areas_resp = self.app.get('/api/v0.9/areas/')
+        self.assertEquals(areas_resp.status_code, 200)
+
+        organizations_resp = self.app.get('/api/v0.9/organizations/')
+        self.assertEquals(organizations_resp.status_code, 200)
+
+        elections_resp = self.app.get('/api/v0.9/elections/')
+        self.assertEquals(elections_resp.status_code, 200)
+
+        posts_resp = self.app.get('/api/v0.9/posts/')
+        self.assertEquals(posts_resp.status_code, 200)
+
+    def test_api_errors(self):
+        response = self.app.get(
+            '/api/',
+            expect_errors=True
+        )
+        self.assertEquals(response.status_code, 404)
+
+        response = self.app.get(
+            '/api/v0.8',
+            expect_errors=True
+        )
+        self.assertEquals(response.status_code, 404)
+
+        response = self.app.get(
+            '/api/v0.9/person/',
+            expect_errors=True
+        )
+        self.assertEquals(response.status_code, 404)
+
+        response = self.app.get(
+            '/api/v0.9/persons/4000/',
+            expect_errors=True
+        )
+        self.assertEquals(response.status_code, 404)
+
+        response = self.app.post(
+            '/api/v0.9/persons/',
+            {},
+            expect_errors=True
+        )
+        self.assertEquals(response.status_code, 403)
+
+    def test_api_persons(self):
+        persons_resp = self.app.get('/api/v0.9/persons/')
+
+        persons = persons_resp.json
+
+        self.assertEqual(persons['count'], len(persons['results']))
+        self.assertEqual(persons['count'], 5)
+
+    def test_api_person(self):
+        person_resp = self.app.get('/api/v0.9/persons/2009/')
+
+        self.assertEqual(person_resp.status_code, 200)
+
+        person = person_resp.json
+        self.assertEqual(person['id'], 2009)
+        self.assertEqual(person['name'], 'Tessa Jowell')
+
+        memberships = person['memberships']
+
+        self.assertEqual(len(memberships), 2)
+        self.assertEqual(memberships[1]['role'], 'Candidate')
+
+        self.assertEqual(len(person['versions']), 0)
+
+    def test_api_areas(self):
+        areas_resp = self.app.get('/api/v0.9/areas/')
+
+        areas = areas_resp.json
+
+        self.assertEqual(areas['count'], len(areas['results']))
+        self.assertEqual(areas['count'], 1)
+
+    def test_api_area(self):
+        areas_resp = self.app.get('/api/v0.9/areas/')
+        areas = areas_resp.json
+
+        area_id = 0
+        for area in areas['results']:
+            if area['identifier'] == '65808':
+                area_id = area['id']
+                break
+
+        area_url = '/api/v0.9/areas/{0}/'.format(area_id)
+        area_resp = self.app.get(area_url)
+        self.assertEquals(area_resp.status_code, 200)
+
+        area = area_resp.json
+        self.assertEquals(area['identifier'], '65808')
+        self.assertEquals(area['name'], 'Dulwich and West Norwood')
+        self.assertEquals(area['type']['name'], 'WMC')
+
+    def test_api_organizations(self):
+        organizations_resp = self.app.get('/api/v0.9/organizations/')
+
+        organizations = organizations_resp.json
+
+        self.assertEqual(organizations['count'], len(organizations['results']))
+        self.assertEqual(organizations['count'], 2)
+
+    def test_api_organization(self):
+        organizations_resp = self.app.get('/api/v0.9/organizations/')
+        organizations = organizations_resp.json
+
+        organization_id = 0
+        for organization in organizations['results']:
+            if organization['slug'] == 'party:53':
+                organization_id = organization['id']
+                break
+
+        organization_url = \
+            '/api/v0.9/organizations/{0}/'.format(organization_id)
+        organization_resp = self.app.get(organization_url)
+        self.assertEquals(organization_resp.status_code, 200)
+
+        organization = organization_resp.json
+        self.assertEquals(organization['slug'], 'party:53')
+        self.assertEquals(organization['name'], 'Labour Party')
+
+    def test_api_elections(self):
+        elections_resp = self.app.get('/api/v0.9/elections/')
+
+        elections = elections_resp.json
+
+        self.assertEqual(elections['count'], len(elections['results']))
+        self.assertEqual(elections['count'], 2)
+
+    def test_api_election(self):
+        elections_resp = self.app.get('/api/v0.9/elections/')
+        elections = elections_resp.json
+
+        election_id = 0
+        for election in elections['results']:
+            if election['slug'] == '2015':
+                election_id = election['id']
+                break
+
+        election_url = '/api/v0.9/elections/{0}/'.format(election_id)
+        election_resp = self.app.get(election_url)
+        self.assertEquals(election_resp.status_code, 200)
+
+        election = election_resp.json
+        self.assertEquals(election['slug'], '2015')
+        self.assertEquals(election['name'], '2015 General Election')
+
+    def test_api_posts(self):
+        posts_resp = self.app.get('/api/v0.9/posts/')
+
+        posts = posts_resp.json
+
+        self.assertEqual(posts['count'], len(posts['results']))
+        self.assertEqual(posts['count'], 2)
+
+    def test_api_post(self):
+        posts_resp = self.app.get('/api/v0.9/posts/')
+        posts = posts_resp.json
+
+        post_id = 0
+        for post in posts['results']:
+            if post['slug'] == '65808':
+                post_id = post['id']
+                break
+
+        self.assertNotEqual(post_id, 0)
+        post_resp = self.app.get('/api/v0.9/posts/{0}/'.format(post_id))
+        self.assertEqual(post_resp.status_code, 200)
+
+        post = post_resp.json
+
+        self.assertEqual(post['slug'], '65808')
+        self.assertEqual(
+            post['label'],
+            'Member of Parliament for Dulwich and West Norwood'
+        )
+
+    def test_api_version_info(self):
+        version_resp = self.app.get('/version.json')
+        self.assertEqual(version_resp.status_code, 200)
+
+        info = version_resp.json
+        self.assertEqual(info['users_who_have_edited'], 0)
+        self.assertEqual(info['interesting_user_actions'], 0)
+
+        LoggedAction.objects.create(
+            action_type='set-candidate-not-elected'
+        )
+
+        LoggedAction.objects.create(
+            action_type='edit-candidate'
+        )
+
+        version_resp = self.app.get('/version.json')
+        info = version_resp.json
+        self.assertEqual(info['interesting_user_actions'], 1)
