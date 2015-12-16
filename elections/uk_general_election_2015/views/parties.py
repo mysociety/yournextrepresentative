baf08cb5aedd7a75dad8f79601ce31244544a3dd
Mark Longair <mhl@pobox.com>
2015-12-16 18:17:49 +0000

    Fix the 'Independent' party pages for UK elections
    There's no Electoral Commission identifier for the 'Independent'
    pseudo-party, so the party page for independents was failing.

--- elections/uk_general_election_2015/views/parties.py
+++ elections/uk_general_election_2015/views/parties.py
@@ -1,15 +1,20 @@
 from candidates.views import PartyDetailView
+from popolo.models import Identifier
 
 class UKPartyDetailView(PartyDetailView):
 
     def get_context_data(self, **kwargs):
         context = super(UKPartyDetailView, self).get_context_data(**kwargs)
 
-        party_ec_id = context['party'].identifiers.get(scheme='electoral-commission')
-        context['ec_url'] = None
-        if party_ec_id:
-            ec_tmpl = 'http://search.electoralcommission.org.uk/English/Registrations/{0}'
-            context['ec_url'] = ec_tmpl.format(party_ec_id)
-        context['register'] = context['party'].extra.register
+        context['ec_url'] = ''
+        context['register'] = ''
+        try:
+            party_ec_id = context['party'].identifiers.get(scheme='electoral-commission')
+            if party_ec_id:
+                ec_tmpl = 'http://search.electoralcommission.org.uk/English/Registrations/{0}'
+                context['ec_url'] = ec_tmpl.format(party_ec_id)
+            context['register'] = context['party'].extra.register
+        except Identifier.DoesNotExist:
+            pass
 
         return context
