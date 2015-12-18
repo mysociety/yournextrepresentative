e479878900fc1d94111143b117c651ac4685ae6f
Mark Longair <mhl@pobox.com>
2015-12-18 13:00:53 +0000

    Add a comment explaining the Minimal* serializer classes

--- candidates/serializers.py
+++ candidates/serializers.py
@@ -8,6 +8,22 @@ from images.models import Image
 from elections import models as election_models
 from popolo import models as popolo_models
 
+# These are serializer classes from the Django-REST-framework API
+#
+# For most objects there are two serializers - a full one and a
+# minimal one.  The minimal ones (whose class names begin 'Minimal')
+# are used for serializing the objects when they're just being
+# included as related objects, rather than the resource that
+# information is being requested about.
+#
+# e.g. if you request information about a Post via the 'posts'
+# endpoint, it's pretty useful to have the ID, URL and name of the
+# elections that the Post is part of, but you probably don't need
+# every bit of election metadata.  A request to the 'elections'
+# endpoint, however, would include full metadata about the elections.
+#
+# This reduces the bloat of API responses, at the cost of some users
+# having to make extra queries.
 
 class OtherNameSerializer(serializers.ModelSerializer):
     class Meta:
