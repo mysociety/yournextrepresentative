610689deca3cb67f57a8d65fdd63af823714aecc
Mark Longair <mhl@pobox.com>
2016-01-07 22:06:35 +0000

    CR: Add a simple smoke test for the front page

---
+++ elections/cr/tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django_webtest import WebTest
+from nose.plugins.attrib import attr
+
+@attr(country='cr')
+class CostaRicaTests(WebTest):
+
+    def test_front_page(self):
+        # Just a smoke test, e.g. to check that all the assets
+        # required are present...
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
