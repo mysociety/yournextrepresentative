fb71723de3c9d90b7de80e411251a37ac9955ff3
Mark Longair <mhl@pobox.com>
2016-01-13 16:09:57 +0000

    CR: add the "Cargos Importantes" field
    There's still some work to do to display this including newlines
    (see #646) but this lets users add text for this option.

---
+++ candidates/migrations/0018_cr_add_important_posts_field.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import models, migrations
+
+def add_extra_field(apps, schema_editor):
+    ExtraField = apps.get_model('candidates', 'ExtraField')
+    if settings.ELECTION_APP == 'cr':
+         ExtraField.objects.create(
+            key='important_roles',
+            type='longer-text',
+            label=u'Important Roles',
+        )
+
+def remove_extra_field(apps, schema_editor):
+    ExtraField = apps.get_model('candidates', 'ExtraField')
+    if settings.ELECTION_APP == 'cr':
+        extra_field = ExtraField.objects.get('important_roles')
+        extra_field.personextrafieldvalue_set.all().delete()
+        extra_field.delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('candidates', '0017_remove_cv_and_program_fields'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_extra_field, remove_extra_field)
+    ]
