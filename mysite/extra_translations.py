d78e84fdcb977e856406e47adcfbeccffad020dc
Mark Longair <mhl@pobox.com>
2016-01-15 12:41:53 +0000

    Add some strings for translation to be picked up by makemessages

---
+++ mysite/extra_translations.py
@@ -0,0 +1,10 @@
+# This module exists just to list strings for translation to be picked
+# up by makemessages.
+
+from django.utils.translation import ugettext as _
+
+# Labels for the extra fields which are defined in the database.
+# Costa Rica:
+_('Profession')
+_('Important Roles')
+_('Standing for re-election')
