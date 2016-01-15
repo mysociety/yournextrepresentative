aa7218ba6bcaa0b03e300b551ba9d38ed8ec831a
Struan Donald <struan@exo.org.uk>
2016-01-15 16:35:49 +0000

    display Yes/No extra fields values on candidate page
    we need to translate the internal value stored for Yes/No fields into
    the correct string in the template

---
+++ candidates/templatetags/extra_field_value.py
@@ -0,0 +1,28 @@
+from django import template
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
+
+register = template.Library()
+
+
+@register.filter
+def extra_field_value(extra_field):
+    if extra_field['value'] == '':
+        return ''
+
+    if extra_field['type'] == 'url':
+        url = conditional_escape(extra_field['value'])
+        output = \
+            u'<a href="{0}" rel="nofollow">{0}</a>'.format(url)
+        output = mark_safe(output)
+    elif extra_field['type'] == 'yesno':
+        if extra_field['value'] == 'yes':
+            output = _(u'Yes')
+        elif extra_field['type'] == 'no':
+            output = _(u'No')
+        else:
+            output = _(u"Don't know")
+    else:
+        output = extra_field['value']
+    return output
