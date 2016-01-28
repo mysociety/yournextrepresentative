7629077753e324e4a80630a9dcc977ba4048b301
Struan Donald <struan@exo.org.uk>
2016-01-28 15:35:30 +0000

    Add SimplePopoloFields to the admin interface
    To allow people to edit and add to them

--- candidates/admin.py
+++ candidates/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm
 
-from .models import LoggedAction, PartySet, ExtraField, PersonExtraFieldValue
+from .models import LoggedAction, PartySet, ExtraField, PersonExtraFieldValue, SimplePopoloField
 # Register your models here.
 
 
@@ -58,7 +58,12 @@ class PersonExtraFieldValueAdmin(admin.ModelAdmin):
     form = PersonExtraFieldValueAdminForm
 
 
+class SimplePopoloFieldAdmin(admin.ModelAdmin):
+    list_display = ['name', 'label']
+
+
 admin.site.register(LoggedAction, LoggedActionAdmin)
 admin.site.register(PartySet, PartySetAdmin)
 admin.site.register(ExtraField, ExtraFieldAdmin)
 admin.site.register(PersonExtraFieldValue, PersonExtraFieldValueAdmin)
+admin.site.register(SimplePopoloField, SimplePopoloFieldAdmin)
