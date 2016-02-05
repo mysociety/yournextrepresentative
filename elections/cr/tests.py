25aa8a94025fe4aea4f1ba966b020784e97ae4e2
Mark Longair <mhl@pobox.com>
2016-02-05 17:18:27 +0000

    fixup! [Unicode]  Unicode string literals everywhere, sans the 'u' prefix

--- elections/cr/tests.py
+++ elections/cr/tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from mock import patch
-
 from __future__ import unicode_literals
+
+from mock import patch
 
 from django_webtest import WebTest
 from nose.plugins.attrib import attr
