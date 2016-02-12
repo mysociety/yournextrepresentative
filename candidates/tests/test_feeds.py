9f97f232a23dab38736e487bd69377b977dff752
Struan Donald <struan@exo.org.uk>
2016-02-12 16:35:33 +0000

    Update feed tests to use a person object when creating LoggedAction
    Otherwise the notification signal attached to LoggedAction for the
    alerts throws an error as it expects a Person to exist

--- candidates/tests/test_feeds.py
+++ candidates/tests/test_feeds.py
@@ -4,17 +4,24 @@ from __future__ import unicode_literals
 
 from django_webtest import WebTest
 
+from popolo.models import Person
 from .auth import TestUserMixin
 from ..models import LoggedAction
 
 class TestFeeds(TestUserMixin, WebTest):
 
     def setUp(self):
+        self.person1 = Person.objects.create(
+            name='Test Person1'
+        )
+        self.person2 = Person.objects.create(
+            name='Test Person2'
+        )
         self.action1 = LoggedAction.objects.create(
             user=self.user,
             action_type='person-create',
             ip_address='127.0.0.1',
-            person_id='9876',
+            person=self.person1,
             popit_person_new_version='1234567890abcdef',
             source='Just for tests...',
         )
@@ -22,7 +29,7 @@ class TestFeeds(TestUserMixin, WebTest):
             user=self.user,
             action_type='candidacy-delete',
             ip_address='127.0.0.1',
-            person_id='1234',
+            person=self.person2,
             popit_person_new_version='987654321',
             source='Something with unicode in it…',
         )
@@ -35,3 +42,5 @@ class TestFeeds(TestUserMixin, WebTest):
     def tearDown(self):
         self.action2.delete()
         self.action1.delete()
+        self.person2.delete()
+        self.person1.delete()
