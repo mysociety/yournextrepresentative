4cef2ddd9801a07b39dbfbd417b9dfa459eb1ec0
Struan Donald <struan@exo.org.uk>
2016-02-16 17:24:48 +0000

    remove translations from model
    Avoid using translations in case it produces unwanted changes in
    migrations and instead move them to the extra_tranlations file

--- mysite/extra_translations.py
+++ mysite/extra_translations.py
@@ -10,3 +10,21 @@ from django.utils.translation import ugettext as _
 _('Profession')
 _('Important Roles')
 _('Standing for re-election')
+
+# Labels for the person fields which are setup in the database and it pulls
+# the label text from the database
+_('Name')
+_('Family Name')
+_('Given Name')
+_('Additional Name')
+_('Honorific Prefix')
+_('Honorific Suffix')
+_('Patronymic Name')
+_('Sort Name')
+_('Email')
+_('Gender')
+_('Birth Date')
+_('Death Date')
+_('Summary')
+_('Biography')
+_('National Identity')
