09a263f9e603f83038ea688c77957036519c824e
Sym Roe <sym.roe@talusdesign.co.uk>
2016-03-31 15:07:50 +0100

    Only show the first 5 results
    With the number of candidates we have, any matches below the first 5 aren't
    normally relevant at all.

--- candidates/views/search.py
+++ candidates/views/search.py
@@ -2,4 +2,25 @@ from haystack.generic_views import SearchView
 
 
 class PersonSearch(SearchView):
-    pass
+    def get(self, request, *args, **kwargs):
+        ret = super(PersonSearch, self).get(request, *args, **kwargs)
+        context = self.get_context_data(**ret.context_data)
+
+        if context['looks_like_postcode']:
+            if not context['object_list']:
+                # This looks like a postcode, and we've found nothing else
+                # so redirect to a postcode view.
+                home_page = reverse('lookup-postcode')
+                return HttpResponseRedirect("{0}?postcode={1}".format(
+                    home_page,
+                    context['query']
+                ))
+
+        return ret
+
+    def get_context_data(self, **kwargs):
+        context = super(PersonSearch, self).get_context_data(**kwargs)
+        context['looks_like_postcode'] = is_valid_postcode(context['query'])
+        # Only return 5 results
+        context['object_list'] = context['object_list'][:5]
+        return context
