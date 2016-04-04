9ad5f279c33339ab00b1fcf90975c085afe0ab43
Mark Longair <mhl@pobox.com>
2016-04-04 12:02:55 +0100

    Add some more text used in migrations which need translation

--- mysite/extra_translations.py
+++ mysite/extra_translations.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # This module exists just to list strings for translation to be picked
 # up by makemessages.
 
@@ -28,3 +30,24 @@ _('Death Date')
 _('Summary')
 _('Biography')
 _('National Identity')
+
+_('Title / pre-nominal honorific (e.g. Dr, Sir, etc.)')
+_('Full name')
+_('Post-nominal letters (e.g. CBE, DSO, etc.)')
+_('Email')
+_('Gender (e.g. “male”, “female”)')
+_('Date of birth (a four digit year or a full date)')
+
+_('User facing description of the information')
+_('Name of the Popolo related type')
+_('Type of HTML field the user will see')
+_('Value to put in the info_type_key e.g. twitter')
+_('Name of the field in the array that stores the value, e.g url for links, value for contact_type, identifier for identifiers')
+
+_('Twitter username (e.g. democlub)')
+_('Twitter username (e.g. democlub)')
+_('Facebook page (e.g. for their campaign)')
+_('Homepage URL')
+_('Wikipedia URL')
+_('LinkedIn URL')
+_("The party's candidate page for this person")
