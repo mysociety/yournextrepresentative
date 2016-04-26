2451c28765ae8a62851869e8f735445b6ddb80a8
Sym Roe <sym.roe@talusdesign.co.uk>
2016-04-26 13:22:52 +0100

    merge conflisting migrations

---
+++ candidates/migrations/0030_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('candidates', '0029_add_ordering_to_fields_meta'),
+        ('candidates', '0028_auto_20160411_1055'),
+    ]
+
+    operations = [
+    ]
