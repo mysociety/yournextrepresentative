e7086637a19fd4b96be3f62b919fc856812abdf7
Struan Donald <struan@exo.org.uk>
2016-04-27 16:18:27 +0100

    Fix searches featuring apostrophes
    The search uses the text field in the index which is generated by
    Haystack and escapes the content as it goes in. Search terms were not
    being escaped which meant that names with an apostrophe in them never
    matched.
    
    This escapes search terms so they should not match the values in the
    text field in the search index.
    
    Fixes #862

--- candidates/views/search.py
+++ candidates/views/search.py
@@ -1,12 +1,29 @@
-from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.utils.html import escape
 
 from haystack.generic_views import SearchView
+from haystack.forms import SearchForm
+
+
+class PersonSearchForm(SearchForm):
+    """
+    When Haystack indexed things into its sythesized text field
+    it HTML escapes everything. This means that we also need to
+    escape our search terms as otherwise names with apostrophes
+    and the like never match as the entry in the search index
+    is O&#39;Reilly and our search term is O'Reilly
+    """
+    def clean_q(self):
+        return escape(self.cleaned_data['q'])
 
 from elections.uk.lib import is_valid_postcode
 
 
 class PersonSearch(SearchView):
+
+    form_class = PersonSearchForm
+
     def get(self, request, *args, **kwargs):
         ret = super(PersonSearch, self).get(request, *args, **kwargs)
         context = self.get_context_data(**ret.context_data)
