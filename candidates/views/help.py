97ac6de625109e481700ab0600aa1de3456be3d9
Mark Longair <mhl@pobox.com>
2016-04-28 17:52:34 +0100

    Use the new election grouping for CSV downloads as well
    This also adds a link the "all candidates" CSV file.

--- candidates/views/help.py
+++ candidates/views/help.py
@@ -11,16 +11,7 @@ class HelpApiView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HelpApiView, self).get_context_data(**kwargs)
 
-        context['current_csv_list'] = []
-        for election_data in Election.objects.current().by_date():
-            context['current_csv_list'].append({'slug': election_data.slug, 'name': election_data.name})
-
-        context['historic_csv_list'] = []
-        current_slugs = [election['slug'] for election in context['current_csv_list']]
-        for election_data in Election.objects.by_date():
-            if election_data.slug not in current_slugs:
-                context['historic_csv_list'].append(
-                    {'slug': election_data.slug, 'name': election_data.name})
+        context['grouped_elections'] = Election.group_and_order_elections()
 
         context['base_api_url'] = self.request.build_absolute_uri(
             reverse('api-root', kwargs={'version': 'v0.9'})
