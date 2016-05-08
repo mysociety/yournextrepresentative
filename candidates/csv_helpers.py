6bf1e8e28a6ae4078ee87a6d61a88595babaa414
Mark Longair <mhl@pobox.com>
2016-05-08 20:08:24 +0100

    Use one of two sorts of candidates in CSV files
    For the "all candidates" file we sort them by last name of the
    candidate; for a single election's CSV file we group them by post.

--- candidates/csv_helpers.py
+++ candidates/csv_helpers.py
@@ -4,15 +4,33 @@ from compat import StreamDictWriter
 from .models import CSV_ROW_FIELDS
 
 
-def _candidate_sort_key(row):
-    return (row['election'], row['post_label'], row['name'].split()[-1])
+def _candidate_sort_by_name_key(row):
+    return (
+        row['name'].split()[-1],
+        not row['election_current'],
+        row['election_date'],
+        row['election'],
+        row['post_label']
+    )
+
+def _candidate_sort_by_post_key(row):
+    return (
+        not row['election_current'],
+        row['election_date'],
+        row['election'],
+        row['post_label'],
+        row['name'].split()[-1])
 
 
-def list_to_csv(candidates_list):
+def list_to_csv(candidates_list, group_by_post=False):
     from .election_specific import EXTRA_CSV_ROW_FIELDS
     csv_fields = CSV_ROW_FIELDS + EXTRA_CSV_ROW_FIELDS
     writer = StreamDictWriter(fieldnames=csv_fields)
     writer.writeheader()
-    for row in sorted(candidates_list, key=_candidate_sort_key):
+    if group_by_post:
+        sorted_rows = sorted(candidates_list, key=_candidate_sort_by_post_key)
+    else:
+        sorted_rows = sorted(candidates_list, key=_candidate_sort_by_name_key)
+    for row in sorted_rows:
         writer.writerow(row)
     return writer.output
