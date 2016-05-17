ea8ed8e060f37ed9ddfa099059ec0bb96c76d8a8
Struan Donald <struan@exo.org.uk>
2016-05-17 16:00:06 +0100

    script to set winners from 2010 UK general election
    This fetches the list of MPs at the 2010 general election from
    TheyWorkForYou and uses the parlparse ID to find the corresponding YNR
    people and set the candidate property on the 2010 candidate membership.
    
    Fixes #931#931#931

---
+++ elections/uk/management/commands/uk_import_2010_winners.py
@@ -0,0 +1,55 @@
+import csv
+import requests
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from popolo.models import Person, Membership
+from elections.models import Election
+
+
+"""
+Grab a list of MPs post 2010 election from TWFY and use that
+to update YNR using parlparse IDs
+"""
+class Command(BaseCommand):
+    help = "Import the list of UK 2010 general election winners"
+
+    def handle(self, **options):
+        # CSV of MPs post 2010 election
+        url = "http://www.theyworkforyou.com/mps/?f=csv&date=2010-06-01"
+        r = requests.get(
+            url,
+            stream=True,
+        )
+        r.raise_for_status()
+        winners = csv.DictReader(r.raw)
+        election = Election.objects.get(slug='2010')
+        count = 0
+        with transaction.atomic():
+            for winner in winners:
+                parlparse_id = 'uk.org.publicwhip/person/{0}'.format(
+                    winner['Person ID']
+                )
+                person = Person.objects.get(
+                    identifiers__scheme='uk.org.publicwhip',
+                    identifiers__identifier=parlparse_id
+                )
+
+                try:
+                    membership = person.memberships.get(
+                        extra__election=election,
+                        role='Candidate',
+                        post__organization__extra__slug='commons',
+                    )
+                except Membership.DoesNotExist:
+                    print "failed to find a membership for {0}".format(
+                        parlparse_id
+                    )
+                    continue
+
+                count += 1
+                membership.extra.elected = True
+                membership.extra.save()
+
+        print "matched {0} memberships".format(count)
