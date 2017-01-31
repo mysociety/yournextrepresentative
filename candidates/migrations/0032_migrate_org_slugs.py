808f9e5ae536837b08391c0c7eb211489d18c12a
Sym Roe <sym.roe@talusdesign.co.uk>
2017-01-31 22:37:12 +0000

    Use Organization.classification in Extra slugs
    This is a bit of a hack, I'm sorry.
    
    Because OrganizationExtra.slug is unique in the DB and we can't
    rely on globally unique slugs for all organisation types, we have to
    use the Organization.classification to prefix the slug.
    
    That way slugs only have to be unique per organisation type.
    
    Alongside the importer change, there is also a migration.
    
    It does a bit of a clean up of classifications too, as sometimes
    they're missing.

---
+++ candidates/migrations/0032_migrate_org_slugs.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+def add_missing_classifications(apps):
+    Organization = apps.get_model('popolo', 'Organization')
+
+    for org in Organization.objects.filter(classification=""):
+        if org.name.endswith(' Police') or org.name.endswith(' Constabulary'):
+            org.classification = "police_area"
+            org.save()
+
+    mapping = {
+        "London Assembly": "gla",
+        "Greater London Authority": "gla",
+        "National Assembly for Wales": "naw",
+        "Northern Ireland Assembly": "nia",
+        "Scottish Parliament": "sp",
+    }
+    for name, classification in mapping.items():
+        Organization.objects.filter(name=name).update(
+            classification=classification)
+
+    for org in Organization.objects.filter(classification=""):
+        org.classification = "local-authority"
+        org.save()
+
+
+def add_classification_to_slugs(apps, schema_editor):
+    add_missing_classifications(apps)
+    OrganizationExtra = apps.get_model('candidates', 'OrganizationExtra')
+
+    for ox in OrganizationExtra.objects.exclude(base__classification="Party"):
+        if not ox.slug.startswith(ox.base.classification):
+            ox.slug = ":".join([
+                ox.base.classification,
+                ox.slug
+            ])
+            ox.save()
+
+def remove_classification_from_slugs(apps, schema_editor):
+    OrganizationExtra = apps.get_model('candidates', 'OrganizationExtra')
+
+    for ox in OrganizationExtra.objects.all():
+        if ox.slug.startswith(ox.base.classification) and ":" in ox.slug:
+            ox.slug = ox.slug.split(":")[1]
+            if not OrganizationExtra.objects.filter(slug=ox.slug).exists():
+                ox.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('candidates', '0031_loggedaction_post'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            add_classification_to_slugs,
+            remove_classification_from_slugs
+        )
+    ]
