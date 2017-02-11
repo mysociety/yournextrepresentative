8905f05506aec812965e8d6cc140a236bedd9733
Mark Longair <mhl@pobox.com>
2017-02-11 12:11:36 +0000

    Add a check function for MembershipExtra & PostExtraElection inconsistency
    At the moment it's possible for the post and election referred to by a
    Membership / MembershipExtra to not have a corresponding
    PostExtraElection, which causes various obscure errors. We should
    prevent these objects from being created in the first place, but in the
    mean time this adds a function to check for whether this problem exists.

--- candidates/tests/test_paired_constraint_check.py
+++ candidates/tests/test_paired_constraint_check.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from popolo.models import Post
 
-from ..models import check_paired_models
+from elections.models import Election
+
+from ..models import (
+    PostExtra, check_paired_models, check_membership_elections_consistent)
+from .factories import (
+    ElectionFactory, MembershipExtraFactory, PersonExtraFactory)
 from .uk_examples import UK2015ExamplesMixin
 
 
@@ -25,3 +30,51 @@ class PairedConstraintCheckTests(UK2015ExamplesMixin, TestCase):
         self.assertEqual(
             check_paired_models(),
             expected_errors)
+
+
+class PostElectionCombinationTests(UK2015ExamplesMixin, TestCase):
+
+    def test_relationship_ok(self):
+        new_candidate = PersonExtraFactory.create(
+            base__name='John Doe'
+        )
+        post_extra = PostExtra.objects.get(slug='14419')
+        election = Election.objects.get(slug='2015')
+        # Create a new candidacy:
+        MembershipExtraFactory.create(
+            base__person=new_candidate.base,
+            base__post=post_extra.base,
+            base__organization=election.organization,
+            election=election,
+        )
+        self.assertEqual(
+            check_membership_elections_consistent(),
+            [])
+
+    def test_membership_extra_not_in_post_election_extra(self):
+        new_candidate = PersonExtraFactory.create(
+            base__name='John Doe'
+        )
+        post_extra = PostExtra.objects.get(slug='14419')
+        election = ElectionFactory.create(
+            slug='2005',
+            name='2005 General Election',
+            for_post_role='Member of Parliament',
+            area_types=(self.wmc_area_type,)
+        )
+        # Create a broken candidacy, where the post / election
+        # combination isn't represented in PostExtraElection
+        # relationships.
+        MembershipExtraFactory.create(
+            base__person=new_candidate.base,
+            base__post=post_extra.base,
+            base__organization=election.organization,
+            election=election,
+        )
+        expected_error = "There was a membership for John Doe ({0}) with " \
+            "post Member of Parliament for Edinburgh East (14419) and " \
+            "election 2005 but there's no PostExtraElection linking " \
+            "them.".format(new_candidate.base.id)
+        self.assertEqual(
+            check_membership_elections_consistent(),
+            [expected_error])
