88c9c21ac6484b2d0bb85e82e1d0124b5cf4fdbf
Mark Longair <mhl@pobox.com>
2017-02-13 19:37:47 +0000

    The post_in_election template tag should work without uk_results
    This branch had changed the implementation of the post_in_election
    template tag so that it failed when the uk_results application had not
    been migrated (e.g. in the non-country-specific tests). This commit
    bring back the generic behaviour as a fallback when uk_results is not
    present in INSTALLED_APPS.

--- candidates/templatetags/standing.py
+++ candidates/templatetags/standing.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from slugify import slugify
 
 from django import template
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db.models import Prefetch
 from django.utils.safestring import mark_safe
@@ -14,26 +15,62 @@ from uk_results.models import CandidateResult
 
 register = template.Library()
 
+
+def get_candidacy(person, election):
+    try:
+        if 'uk_results' in settings.INSTALLED_APPS:
+            memberships_qs = person.memberships.prefetch_related(
+                Prefetch(
+                    'result',
+                    CandidateResult.objects.select_related(
+                        'result_set',
+                        'result_set__post_result',
+                    )
+                )
+            )
+        else:
+            memberships_qs = person.memberships.all()
+        return memberships_qs.get(
+                role=election.candidate_membership_role,
+                extra__election=election
+         )
+    except Membership.DoesNotExist:
+        return None
+
+
+def get_known_candidacy_prefix_and_suffix(candidacy):
+    # The 'result' attribute will only be present if the 'uk_results'
+    # application is in INSTALLED_APPS.
+    prefix = ''
+    suffix = ''
+    if 'uk_results' in settings.INSTALLED_APPS:
+        candidate_results = list(candidacy.result.all())
+        if candidate_results:
+            for candidate_result in candidate_results:
+                if candidate_result.result_set.post_result.confirmed:
+                    # Then we're OK to display this result:
+                    suffix += '<br>'
+                    if candidate_result.is_winner:
+                        suffix += '<span class="candidate-result-confirmed candidate-result-confirmed-elected">Elected!</span>'
+                    else:
+                        suffix += '<span class="candidate-result-confirmed candidate-result-confirmed-not-elected">Not elected</span>'
+                    suffix += ' <span class="vote-count">({0} votes)</span>'.format(
+                         candidate_result.num_ballots_reported
+                    )
+                    suffix += '<br>'
+    elif candidacy.extra.party_list_position:
+        prefix += u'<div title="{0}" aria-label="{0}" class="person-position">{1}</div>'.format(
+            _(u'Party list position'),
+            candidacy.extra.party_list_position,
+        )
+    return prefix, suffix
+
+
 @register.filter
 def post_in_election(person, election):
     # FIXME: this is inefficient, but similar to the previous
     # implementation.
-    try:
-        candidacy = person.memberships.prefetch_related(
-            Prefetch(
-                'result',
-                CandidateResult.objects.select_related(
-                    'result_set',
-                    'result_set__post_result',
-                )
-            )
-        ).get(
-            role=election.candidate_membership_role,
-            extra__election=election
-        )
-    except Membership.DoesNotExist:
-        candidacy = None
-
+    candidacy = get_candidacy(person, election)
     if candidacy:
         link = '<a href="{cons_url}">{cons_name}</a>'.format(
             cons_url=reverse(
@@ -52,21 +89,8 @@ def post_in_election(person, election):
         result += ' <span class="party">{0}</span>'.format(
             candidacy.on_behalf_of.name
         )
-        candidate_results = list(candidacy.result.all())
-        if candidate_results:
-            for candidate_result in candidate_results:
-                if candidate_result.result_set.post_result.confirmed:
-                    # Then we're OK to display this result:
-                    result += '<br>'
-                    if candidate_result.is_winner:
-                        result += '<span class="candidate-result-confirmed candidate-result-confirmed-elected">Elected!</span>'
-                    else:
-                        result += '<span class="candidate-result-confirmed candidate-result-confirmed-not-elected">Not elected</span>'
-                    result += ' <span class="vote-count">({0} votes)</span>'.format(
-                         candidate_result.num_ballots_reported
-                    )
-                    result += '<br>'
-
+        prefix, suffix = get_known_candidacy_prefix_and_suffix(candidacy)
+        result = prefix + result + suffix
     else:
         if election in person.extra.not_standing.all():
             if election.current:
