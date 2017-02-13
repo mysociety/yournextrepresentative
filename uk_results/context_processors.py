8db78d750a8e043a25367b6864335de9aff14466
Mark Longair <mhl@pobox.com>
2017-02-13 19:12:48 +0000

    Factor out the setting of election-specific Django settings
    This isolates the addition of settings which are taken (or may be taken)
    from the election application which is in use.

--- uk_results/context_processors.py
+++ uk_results/context_processors.py
@@ -1,10 +1,8 @@
 from django.conf import settings
 
-from auth_helpers.views import user_in_group
-
 def show_results_feature(request):
-    in_group = user_in_group(request.user,
-        "Trusted to confirm control results")
+    in_group = request.user.groups.filter(
+        name='Trusted to confirm control results').exists()
     results_feature_active = getattr(settings, 'RESULTS_FEATURE_ACTIVE', False)
 
     return {
