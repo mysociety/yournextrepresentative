da1642c3422ed980b92fa030374826ca8b5df37e
Mark Longair <mhl@pobox.com>
2017-02-15 10:50:43 +0000

    Fix relative imports for Python 3

--- uk_results/models/__init__.py
+++ uk_results/models/__init__.py
@@ -1,6 +1,6 @@
-from control_models import *
-from votes_models import *
-from map_models import *
+from .control_models import *
+from .votes_models import *
+from .map_models import *
 
 TRUSTED_TO_CONFIRM_CONTROL_RESULTS_GROUP_NAME = \
     "Trusted to confirm control results"
