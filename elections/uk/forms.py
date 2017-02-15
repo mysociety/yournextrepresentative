071a9e8968635f24ad1ca6f44f3b4bd1ca541951
Mark Longair <mhl@pobox.com>
2017-02-15 09:51:05 +0000

    Also catch BadPostCodeExcpetion when validating a postcode

--- elections/uk/forms.py
+++ elections/uk/forms.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from django import forms
 from django.core.exceptions import ValidationError
 
-from candidates.mapit import BaseMapItException
+from candidates.mapit import BaseMapItException, BadPostcodeException
 from popolo.models import Area
 
 from compat import text_type
@@ -26,6 +26,6 @@ class PostcodeForm(forms.Form):
             # contained in a constituency. (If it's valid then the
             # result is cached, so this doesn't cause a double lookup.)
             get_areas_from_postcode(postcode)
-        except BaseMapItException as e:
+        except (BaseMapItException, BadPostcodeException) as e:
             raise ValidationError(text_type(e))
         return postcode
