799963349f1ac02d1e357a4b6dc674c457332a62
Sym Roe <sym.roe@talusdesign.co.uk>
2017-04-13 09:25:25 +0100

    Validate that usernames don't look like email addresses

--- mysite/settings/conf.py
+++ mysite/settings/conf.py
@@ -287,7 +287,7 @@ def get_settings(conf_file_leafname, election_app=None, tests=False):
         'ACCOUNT_EMAIL_VERIFICATION': 'mandatory',
         'ACCOUNT_EMAIL_REQUIRED': True,
         'ACCOUNT_USERNAME_REQUIRED': True,
-        'ACCOUNT_AUTHENTICATION_METHOD': "username_email",
+        'ACCOUNT_USERNAME_VALIDATORS': 'mysite.helpers.allauth_validators',
         'SOCIALACCOUNT_AUTO_SIGNUP': True,
 
         'ROOT_URLCONF': 'mysite.urls',
