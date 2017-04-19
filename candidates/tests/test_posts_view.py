fad698b4c2ab5bdddbfb2f03aa053a2c80c83821
Mark Longair <mhl@pobox.com>
2017-04-19 16:26:32 +0100

    Link to all constituencies in an election from election names on /posts
    As requested by Andy Lulham (@andylolz).

--- candidates/tests/test_posts_view.py
+++ candidates/tests/test_posts_view.py
@@ -23,6 +23,18 @@ class TestPostsView(UK2015ExamplesMixin, WebTest):
             )
         )
 
+    def test_elections_link_to_constituencies_page(self):
+
+        response = self.app.get('/posts')
+
+        heading = response.html.find('h4', text='2015 General Election')
+        heading_children = list(heading.children)
+        self.assertEqual(len(heading_children), 1)
+        expected_link_element = heading_children[0]
+        self.assertEqual(expected_link_element.name, 'a')
+        self.assertEqual(expected_link_element['href'], '/election/2015/constituencies')
+        self.assertEqual(expected_link_element.text, '2015 General Election')
+
     def test_two_elections_posts_page(self):
 
         self.earlier_election.current = True
