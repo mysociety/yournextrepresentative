23f164f2fb3703ae589f00fa401a1a357b5b7d10
Mark Longair <mhl@pobox.com>
2017-05-02 11:17:34 +0100

    Add a __repr__ method for LoggedAction

--- candidates/models/db.py
+++ candidates/models/db.py
@@ -27,6 +27,10 @@ class LoggedAction(models.Model):
     source = models.TextField()
     post = models.ForeignKey(Post, blank=True, null=True)
 
+    def __repr__(self):
+        fmt = str("<LoggedAction username='{username}' action_type='{action_type}'>")
+        return fmt.format(username=self.user.username, action_type=self.action_type)
+
 
 class PersonRedirect(models.Model):
     '''This represents a redirection from one person ID to another
