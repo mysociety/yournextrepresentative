6c1ce400462f382beeca91111977ae9ade8c5d3a
Sym Roe <sym.roe@talusdesign.co.uk>
2017-05-04 20:12:34 +0100

    Only return results for this set of elections

--- results/feeds.py
+++ results/feeds.py
@@ -19,7 +19,7 @@ class BasicResultEventsFeed(Feed):
     description = _("A basic feed of election results")
 
     def items(self):
-        return ResultEvent.objects.filter(created__gt="2016-05-04")
+        return ResultEvent.objects.filter(created__gte="2017-05-04")
 
     def item_title(self, item):
         return _('{name} ({party}) won in {cons}').format(
