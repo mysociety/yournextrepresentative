5916a81e4846e30e61740791778558d10f3e2171
Mark Longair <mhl@pobox.com>
2017-05-18 11:07:18 +0100

    Add a helper: given an image URL, download it to a temporary file
    This will be useful in implementing the "upload a photo from its URL"
    feature.

--- candidates/management/images.py
+++ candidates/management/images.py
@@ -1,8 +1,12 @@
 from __future__ import unicode_literals
 
 import hashlib
+from tempfile import NamedTemporaryFile
+
+from django.utils.translation import ugettext_lazy as _
 
 from PIL import Image as PillowImage
+import requests
 
 
 def get_file_md5sum(filename):
@@ -31,3 +35,31 @@ def get_image_extension(image_filename):
                 return None
             raise
         return PILLOW_FORMAT_EXTENSIONS[pillow_image.format]
+
+
+class ImageDownloadException(Exception):
+    pass
+
+
+def download_image_from_url(image_url):
+    """This downloads an image to a temporary file and returns the filename
+
+    It raises an ImageDownloadException if a GET for the URL results
+    in a HTTP response with status code other than 200, or the
+    downloaded resource doesn't seem to be an image. It's the
+    responsibility of the caller to delete the image once they're
+    finished with it."""
+    with NamedTemporaryFile(delete=False) as image_ntf:
+        image_response = requests.get(image_url)
+        if image_response.status_code != 200:
+            msg = _("  Ignoring an image URL with non-200 status code "
+                    "({status_code}): {url}")
+            raise ImageDownloadException(
+                msg.format(status_code=image_response.status_code, url=image_url))
+        image_ntf.write(image_response.content)
+    # Trying to get the image extension checks that this really is
+    # an image:
+    if get_image_extension(image_ntf.name) is None:
+        msg = _("  The image at {url} wasn't of a known type")
+        raise ImageDownloadException(msg.format(url=image_url))
+    return image_ntf.name
