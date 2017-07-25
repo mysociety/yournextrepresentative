95ebac75381580fe3767d7c9e287226a75bc43bf
Mark Longair <mhl@pobox.com>
2017-07-25 14:00:10 +0100

    KE: Implement post label abbreviation for current posts

--- elections/kenya/lib.py
+++ elections/kenya/lib.py
@@ -3,4 +3,4 @@ from __future__ import unicode_literals
 import re
 
 def shorten_post_label(post_label):
-    return re.sub(r'^Member of Parliament for ', '', post_label)
+    return re.sub(r'^(County Assembly Member for|Member of the National Assembly for|County Governor for|Women Representative for|Senator for|President of)\s+', '', post_label)
